5f0235dff3ddbab4720b8b6347542ba9b44af8e6
Firose Hussain <github@firumon.com>
2019-06-13 15:11:19 +0530

    For uploading file

---
+++ app/assets/scripts/services/formdata.js
@@ -0,0 +1,20 @@
+function getActivityUploadConfig(){ return { url,type:'post',enctype:'multipart/form-data',processData:false,contentType:false } }
+
+class AppFormData {
+    constructor() {}
+    init(){ this.vFormData = new FormData(); }
+    file(data,name){
+        this.vFormData.append('file',new Blob([JSON.stringify(data)],{ type:'application/json' }),[(name || 'table'),'json'].join('.'));
+        return this;
+    }
+    params(data){
+        _.forEach(data,(val,fld) => { this.vFormData.append(fld,val); });
+        return this;
+    }
+    get(){ return this.vFormData; }
+    config(url){
+        return { url,type:'post',enctype:'multipart/form-data',processData:false,contentType:false }
+    }
+}
+
+export const FD = new AppFormData();
