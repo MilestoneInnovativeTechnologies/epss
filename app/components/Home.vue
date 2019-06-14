80eec74515f5f2683aa7b5c1292ec29c776ddf1b
Firose Hussain <github@firumon.com>
2019-06-14 23:46:16 +0530

    Debug

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -3,6 +3,9 @@
     <App title="User Name">
         <InfoWithIcon width="90%" heading="Stores" icon="store_mall_directory" :contents="['Store 1 Name','Store 2 Name']"></InfoWithIcon>
         <InfoWithIcon width="90%" heading="Areas" icon="map" :contents="['Area 1 Name','Area 2 Name']"></InfoWithIcon>
+        <Button @tap="ftb" text="Fet tables" />
+        <ListPicker :items="tbls" v-model="stbl" />
+        <Button @tap="ftbd" text="Fet dets" />
         <template v-for="(items,caption,sidx) in menus">
             <TextTitleSub class="m-t-12 m-b-8 m-l-2">{{ caption }}</TextTitleSub>
             <GridMenuRow :menus="items"></GridMenuRow>
@@ -17,15 +20,30 @@
         computed: {
             ...mapGetters('Menu', ['menus']),
         },
+        data(){ return {
+            tbls:['area_users','areas','fiscalyearmaster','functiondetails','pricelist_header','pricelist','product_transaction_natures','product_transaction_types','productgroups','products','sales_order','sales_order_items','settings','stores','store_product_transactions','transactions','transaction_details','stock_transfer','user_settings','user_store_area','users'],
+            stbl:0,
+            recs:[]
+        }},
+        methods: {
+            ftb(){
+                let vm = this;
+                DB.get('sqlite_master',null,function (vm) { _.forEach(this.result,(ob)=>{
+                    vm.tbls.push(ob.tbl_name);
+                }) },vm)
+            },
+            ftbd(){
+                let vm = this;
+                DB.get(this.tbls[this.stbl],null,function(vm){
+                    vm.recs = this.result;
+                    console.log('has ' + this.result.length + ' records');
+                    console.log(_.head(this.result));
+                },vm)
+            }
+        },
         created() {
             console.log('Home created');
-            DB.get('sqlite_master',null,function () {
-                _.forEach(this.result,function (ob) {
-                    DB.get(ob.tbl_name,null,function () {
-                        console.log(ob.tbl_name+' has '+this.result.length+' records');
-                    })
-                })
-            })
+            this.tbls.push('transactions');
         }
     }
 </script>
--- app/components/Land.vue
+++ app/components/Land.vue
@@ -20,42 +20,13 @@
             ...mapGetters('Log', ['pick']),
             ...mapState('Organization', { OrgImg:'image',OrgName:'name',OrgBrief:'brief',OrgUUID:'uuid' }),
             ...mapState('Sync', { SyncTable:'tables',SyncQueue:'queue',SyncQueueIndex:'queue_index',SyncStartAfter:'app_sync_start_after',SyncRepeatFailedProcSec:'repeat_failed_processing_seconds',SyncProcessing:'processing',SyncURL:'url',SyncTime:'time',SyncRepeatTO:'repeat_failed_timeout',SyncTimeout:'time_out'}),
-            ...mapState('Axios',{ AxiosProcessing:'processing',AxiosTransfer:'transfer',AxiosTransfer:'transfer',AxiosStatus:'status',AxiosSuccess:'success',AxiosFail:'fail' }),
+            ...mapState('Axios',{ AxiosProcessing:'processing',AxiosTransfer:'transfer',AxiosTransfer:'transfer',AxiosStatus:'status',AxiosSuccess:'success',AxiosFail:'fail',AxiosQueue:'queue',AxiosLastResp:'last_response' }),
+        },
+        methods: {
+            huo(){
+                //DB.insert('areas',{ name:'TEST' });
+                DB.create('abc','name,text')
+            }
         }
     }
 </script>
-
-<style scoped>
-    ActionBar {
-        background-color: #FFA656;
-        color: #000000;
-        font-weight: bold;
-    }
-
-    .title {
-        text-align: left;
-        padding-left: 16;
-    }
-
-    .message {
-        vertical-align: center;
-        text-align: center;
-        font-size: 20;
-        color: #333333;
-    }
-
-    .drawer-header {
-        padding: 50 16 16 16;
-        margin-bottom: 16;
-        background-color: #53ba82;
-        color: #ffffff;
-        font-size: 24;
-    }
-
-    .drawer-item {
-        padding: 8 16;
-        color: #333333;
-        font-size: 16;
-    }
-
-</style>
