3b94afad5c1f86220e945716fad1b33e05ce5608
Firose Hussain <github@firumon.com>
2019-06-16 07:58:55 +0530

    Logs added for debug

--- app/assets/scripts/services/formdata.js
+++ app/assets/scripts/services/formdata.js
@@ -1,18 +1,21 @@
-function getActivityUploadConfig(){ return { url,type:'post',enctype:'multipart/form-data',processData:false,contentType:false } }
-
 class AppFormData {
-    constructor() {}
-    init(){ this.vFormData = new FormData(); return this; }
+    constructor() { console.log('FD Constr'); }
+    init(){ this.vFormData = new FormData(); console.log('FD init'); return this; }
     file(data,name){
+        console.log('FD File');
         this.vFormData.append('file',new Blob([JSON.stringify(data)],{ type:'application/json' }),[(name || 'table'),'json'].join('.'));
+        console.log('FD file after');
         return this;
     }
     params(data){
+        console.log('FD params');
         _.forEach(data,(val,fld) => { this.vFormData.append(fld,val); });
+        console.log('FD params after');
         return this;
     }
-    get(){ return this.vFormData; }
+    get(){ console.log('FD get'); return this.vFormData; }
     config(url){
+        console.log('FD config');
         return { url,type:'post',enctype:'multipart/form-data',processData:false,contentType:false }
     }
 }
