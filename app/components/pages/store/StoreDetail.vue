1e32e9f9c1556dc974ab1aeb37d544118aea1b7f
Firose Hussain <github@firumon.com>
2019-06-17 21:40:54 +0530

    Debug

---
+++ app/components/pages/store/StoreDetail.vue
@@ -0,0 +1,15 @@
+<template>
+    <App title="Store Details">
+        <HtmlView :html="html"></HtmlView>
+    </App>
+</template>
+
+<script>
+    export default {
+        name: "StoreDetail",
+        props: ['id'],
+        computed: {
+            html(){ return `<h1>Store ${this.id}</h1>` }
+        }
+    }
+</script>
--- app/components/pages/store/StoreIndex.vue
+++ app/components/pages/store/StoreIndex.vue
@@ -1,13 +1,18 @@
+<!--suppress ALL -->
 <template>
-    <App title="Stores"></App>
+    <App title="Stores">
+        <Label height="50"> YoYo </Label>
+        <AppList :data="stores" detail="store/StoreDetail" :props="['id','created_at']" :layout="{ ID:'id',Name:'name',Created:'created_at',UPDATED:'updated_at' }" :links="{ updated_at:['store/StoreDetail','id','updated_at'] }"></AppList>
+    </App>
 </template>
 
 <script>
+    import { mapGetters } from 'vuex';
     export default {
-        name: "StoreIndex"
+        name: "StoreIndex",
+        computed: {
+            ...mapGetters('Stores',['_dataById']),
+            stores(){ return this._dataById('stores'); }
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
