365437334b653ba03372297a1e63ac1dd0e9c0b9
Firose Hussain <github@firumon.com>
2019-06-17 21:40:40 +0530

    New component

---
+++ app/components/typography/TextHeadingRowSub.vue
@@ -0,0 +1,10 @@
+<template>
+    <TextBold :text="mText" class="fs12 text-uppercase c-grey-light" />
+</template>
+
+<script>
+    export default {
+        name: "TextHeadingRowSub",
+        mixins: [require('./../../assets/scripts/mixins/typography').default],
+    }
+</script>
