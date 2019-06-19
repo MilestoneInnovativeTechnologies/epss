4cf0d0529ddc334570d9bb03868c9e6d977aa7e9
Firose Hussain <github@firumon.com>
2019-06-19 08:58:43 +0530

    Reformat

--- app/assets/scripts/vuex/modules/database/actions.js
+++ app/assets/scripts/vuex/modules/database/actions.js
@@ -3,26 +3,24 @@ import {
     table_information_db_table_fields,
     table_information_db_table_name
 } from './../../constants';
-import { add_new_table_for_sync } from './../../mutation-types';
 
 export function Setup({ dispatch }){
     log('Initializing Database fetch');
     dispatch('get',{ url:database_fetch_url,success:'Database/create' },{ root:true })
 }
 
-export function create({ dispatch,commit }, data) {
-    DB.create(table_information_db_table_name, table_information_db_table_fields,function(data, dispatch){
-        if(this.error) return log(`Error in creating ${table_information_db_table_name} db table`,this.result);
-        _.forEach(data,(Ary,Table) => {
-            log('Creating Table: ' + Table);
-            DB.create(Table,Ary[0],function(table,fields,type,down,up,dispatch,commit){
-                log('Created Table: ' + table); log('Inserting Table Information for, ' + table);
-                DB.insert(table_information_db_table_name,{ table,fields,type,down,up },function(table,type,dispatch){
-                    if (type === 'APP') dispatch('Sync/downloadTableRecords',table,{ root:true });
-                    },table,type,dispatch);
-                commit('Sync/' + add_new_table_for_sync,{ table,up,down,type },{ root:true })
-                },Table,Ary[0],Ary[1],_.toSafeInteger(parseInt(Ary[2])*3600),_.toSafeInteger(parseInt(Ary[3])*60),dispatch,commit);
-
+export function create({dispatch}, data) {
+    DB.create(table_information_db_table_name, table_information_db_table_fields, function (data, dispatch) {
+        if (this.error) return log(`Error in creating ${table_information_db_table_name} db table`, this.result);
+        _.forEach(data, (Ary, Table) => {
+            if (!Table) return; log('Creating Table: ' + Table);
+            DB.create(Table, Ary[0], function (table, fields, type, down, up, dispatch) {
+                log('Created, ' + table + ', Inserting Information');
+                let insertData = {table, fields, type, down, up};
+                DB.insert(table_information_db_table_name, insertData, function (tblObj, dispatch) {
+                    if (tblObj.type === 'APP') dispatch('Sync/downloadTableRecords', tblObj, {root: true});
+                }, insertData, dispatch);
+            }, Table, Ary[0], Ary[1], _.toSafeInteger(parseInt(Ary[2]) * 3600), _.toSafeInteger(parseInt(Ary[3]) * 60), dispatch);
         });
-        },data,dispatch,commit);
+    }, data, dispatch);
 }
