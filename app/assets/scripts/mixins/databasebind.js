c5f877fe7f07e6ad9ee41992607c1e47ba1be6e3
Firose Hussain <github@firumon.com>
2019-06-20 11:02:15 +0530

    Common getter, for deep relation fetching implemented

--- app/assets/scripts/mixins/databasebind.js
+++ app/assets/scripts/mixins/databasebind.js
@@ -34,11 +34,41 @@ export default {
         __properTable({ _table }){
             return (table) => table || (_.isArray(_table) ? _table[0] : _table)
         },
-        _tableData({ _data }){
-            return (table) => _data[table];
+        _tableData({ _data },{ __properTable }){
+            return (table) => _data[__properTable(table)];
         },
-        _dataById(state,{ __properTable,_tableData }){
-            return (table,id) => _.keyBy(_tableData(__properTable(table)),(id || 'id'))
+        _tableDataById(state,{ _tableData }){
+            return (table,id) => { return _.keyBy(_tableData(table),(id || 'id')) }
+        },
+        _tableDataByIdField(state,{ _tableDataById }){
+            return (table,field) => { return _.mapValues(_tableDataById(table),(field || 'name')) }
+        },
+        _tableDataByIdName(state,{ _tableDataByIdField }){
+            return (table) => { return _tableDataByIdField(table,'name') }
+        },
+        _tableDataItemByKey(state,{ _tableData }){
+            return (table,key,value) => { return _.find(_tableData(table),mpkva(key,value)) }
+        },
+        _tableDataItem(state,{ _tableDataItemByKey }){
+            return (table,id) => { return _tableDataItemByKey(table,'id',id) }
+        },
+        _tableDataFilter(state,{ _tableData }){
+            return (table,key,value) => _.filter(_tableData(table),mpkva(key,value))
+        },
+        _tableDataItemRelation(state,{ _tableDataItem }){
+            //[prdId,[['products','group1'],['productgroups','tax1'],'tax']]
+            return (id,relationDeep,table) => { return _tableDataItem(table,_.reduce(relationDeep,function(id,relation){
+                return _.get(_tableDataItem(relation[0],id),relation[1])
+            },id)) }
         },
     }
+}
+
+function matchesPropertyKeyValueArray(key,value){
+    let intKeys = ['id','created_at','updated_at'];
+    let val = _.includes(intKeys,key) ? _.toSafeInteger(value) : _.toString(value);
+    return [key,val];
+}
+function mpkva(key, value) {
+    return matchesPropertyKeyValueArray(key, value)
 }
