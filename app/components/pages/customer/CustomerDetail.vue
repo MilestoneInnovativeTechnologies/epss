6bdfd31f5fe1f77ccff9e044f7268a79d4e2e085
Firose Hussain <github@firumon.com>
2019-06-20 19:59:04 +0530

    Layout corrected

--- app/components/pages/customer/CustomerDetail.vue
+++ app/components/pages/customer/CustomerDetail.vue
@@ -1,15 +1,16 @@
 <template>
-    <App title="Customer Detail"></App>
-<!--    <TextTitle>{{ customer.name }}</TextTitle>-->
-<!--    <TextTitleSub>{{ customer.code }}</TextTitleSub>-->
-<!--    <GridLayout rows="auto" columns="2*,*">-->
-<!--        <TextBold row="0" col="0">{{ customer.phone }}</TextBold>-->
-<!--        <TextHeadingRowSub row="0" col="1">Phone</TextHeadingRowSub>-->
-<!--    </GridLayout>-->
-<!--    <GridLayout rows="auto" columns="2*,*">-->
-<!--        <TextBold row="0" col="0">{{ customer.email }}</TextBold>-->
-<!--        <TextHeadingRowSub row="0" col="1">Email</TextHeadingRowSub>-->
-<!--    </GridLayout>-->
+    <App title="Customer Detail">
+        <TextTitle>{{ customer.name }}</TextTitle>
+        <TextTitleSub>{{ customer.code }}</TextTitleSub>
+            <GridLayout rows="auto" columns="2*,*">
+                <TextBold row="0" col="0" class="text-left">{{ customer.phone }}</TextBold>
+                <TextHeadingRowSub row="0" col="1" class="text-right">Phone</TextHeadingRowSub>
+            </GridLayout>
+            <GridLayout rows="auto" columns="2*,*">
+                <TextBold row="0" col="0" class="text-left">{{ customer.email }}</TextBold>
+                <TextHeadingRowSub row="0" col="1" class="text-right">Email</TextHeadingRowSub>
+            </GridLayout>
+    </App>
 </template>
 
 <script>
@@ -22,7 +23,7 @@
 
         } },
         computed:{
-            ...mapGetters('Customer',['_tableDataItem','customers']),
+            ...mapGetters('Customer',['_tableDataItem']),
             customer(){ return this._tableDataItem('users',this.id) },
         },
         methods: {
