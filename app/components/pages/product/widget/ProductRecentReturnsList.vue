62e01590cee97849d8a237697a8ae78767986010
Firose Hussain <github@firumon.com>
2019-06-20 12:17:43 +0530

    Corrected

--- app/components/pages/product/widget/ProductRecentReturnsList.vue
+++ app/components/pages/product/widget/ProductRecentReturnsList.vue
@@ -1,5 +1,5 @@
 <template>
-    <AppList title="Recent Returns" :limit="limit" :source="recentReturnsSource"></AppList>
+    <AppList title="Recent Returns" :limit="limit" :source="recentReturnsSource" :layout="layout" :links="{ customer:['customer/CustomerDetail',{ id:'cid' }] }"></AppList>
 </template>
 
 <script>
@@ -8,6 +8,9 @@
     export default {
         name: "ProductRecentReturnsList",
         props: ['id','limit'],
+        data(){ return {
+            layoutTemplate:['docno','customer','date','quantity','nature']
+        }},
         computed: {
             ...mapGetters('Product',['productTrans']),
             ...mapGetters('TRNS',{ naturesIdName: '_tableDataByIdName' }), ...mapGetters('TRPS',{ typesIdName: '_tableDataByIdName' }),
@@ -20,10 +23,11 @@
             rProdTrans(){ let rTypeId = this.transTypes.Return; return _.filter(this.transactions,(trns) => _.get(trns,'type') === rTypeId) },
             recentReturnsSource(){ let vm = this; return _.mapValues(vm.rProdTrans,(rProdTran) => {
                 if(!rProdTran) return null; let rTran = vm.getReturnDetails(rProdTran._ref); if(!rTran) return null;
-                let date = moment(rProdTran.date).format(__.DOCDATE_FORMAT), nature = _.get(vm.transNatures,rProdTran.nature),
-                    customer = _.get(vm.users,rTran.customer), docno = rTran.docno, quantity =_.round(rProdTran.quantity,__.QUANTITY_DECIMAL);
-                return { docno,customer,date,quantity,nature }
-            }) }
+                let date = moment(rProdTran.date).format(__.DOCDATE_FORMAT), nature = _.get(vm.transNatures,rProdTran.nature), cid = rTran.customer,
+                    customer = _.get(vm.users,cid), docno = rTran.docno, quantity =_.round(rProdTran.quantity,__.QUANTITY_DECIMAL);
+                return { docno,customer,date,quantity,nature,cid }
+            }) },
+            layout(){ return _.mapKeys(this.layoutTemplate,(item) => _.capitalize(item))},
         },
         methods: {
             getReturnDetails(spt){ return this.sptTransactionDetails(spt) }
