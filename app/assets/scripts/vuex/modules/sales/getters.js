7ba61bc8173e73aa639b15c5c3ef6a829de7f365
Firose Hussain <github@firumon.com>
2019-06-20 23:28:28 +0530

    Customer getter reformat

--- app/assets/scripts/vuex/modules/sales/getters.js
+++ app/assets/scripts/vuex/modules/sales/getters.js
@@ -1,3 +1,9 @@
-export function data(state) {
-    return state.data;
+export function customer(state,{ _tableDataFilter,_tableDataByGroup,_tableDataById }) {
+    return (customer) => {
+        let cusTrans = _tableDataFilter('transactions', 'customer', customer);
+        let cusSaleTrans = _.filter(cusTrans,(item) => _.startsWith(item.fncode,'SL'));
+        let transDetails = _.pick(_tableDataByGroup('transaction_details','transaction'),_.flatMap(cusSaleTrans,'_ref'));
+        let totals = _.mapValues(transDetails,(detArray) => _.sum(_.map(detArray,(arrayItem) => _.toNumber(arrayItem.total))));
+        return _.map(cusSaleTrans,(trans) => { return { ...trans,total:_.get(totals,trans._ref) }})
+    }
 }
