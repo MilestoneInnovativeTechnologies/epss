ee2b35d876686b07e8af9630b93b29c94cfd36b0
Firose Hussain <github@firumon.com>
2019-06-20 20:14:54 +0530

    AppInfoWide component created..

---
+++ app/components/app/info/AppInfoWide.vue
@@ -0,0 +1,15 @@
+<template>
+    <GridLayout rows="auto" columns="2*,*">
+        <TextBold row="0" col="0" class="text-left"><slot></slot></TextBold>
+        <TextHeadingRowSub row="0" col="1" class="text-right">{{ title }}</TextHeadingRowSub>
+    </GridLayout>
+</template>
+
+<script>
+    export default {
+        name: "AppInfoWide",
+        props: {
+            title: { default:'' },
+        }
+    }
+</script>
