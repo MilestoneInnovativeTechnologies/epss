ce5b4907e50f5393f322188a304eb01d867dfde3
Firose Hussain <github@firumon.com>
2019-06-21 16:08:02 +0530

    Home widgets updated

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,12 +1,12 @@
 <!--suppress ALL -->
 <template>
     <App title="User Name">
-<!--        <UserStoresInfoWithIcon width="90%"></UserStoresInfoWithIcon>-->
-<!--        <UserAreasInfoWithIcon width="90%"></UserAreasInfoWithIcon>-->
-<!--        <TextTitleSub width="90%" class="m-t-20 cp">Outstandings</TextTitleSub>-->
-<!--        <UserCustomerTotalOutstandingMetric width="90%"></UserCustomerTotalOutstandingMetric>-->
-<!--        <TextTitleSub width="90%" class="m-t-20 cp">Sales Orders</TextTitleSub>-->
-<!--        <SalesOrderPendingMetric width="90%" class="m-b-10"></SalesOrderPendingMetric>-->
+        <UserStoresInfoWithIcon width="90%"></UserStoresInfoWithIcon>
+        <UserAreasInfoWithIcon width="90%"></UserAreasInfoWithIcon>
+        <TextTitleSub width="90%" class="m-t-20 cp">Outstandings</TextTitleSub>
+        <UserCustomerTotalOutstandingMetric width="90%"></UserCustomerTotalOutstandingMetric>
+        <TextTitleSub width="90%" class="m-t-20 cp">Sales Orders</TextTitleSub>
+        <SalesOrderPendingMetric width="90%" class="m-b-10"></SalesOrderPendingMetric>
         <template v-for="(items,caption,sidx) in menus">
             <TextTitleSub class="m-t-12 m-b-8 m-l-2">{{ caption }}</TextTitleSub>
             <GridMenuRow :menus="items"></GridMenuRow>
