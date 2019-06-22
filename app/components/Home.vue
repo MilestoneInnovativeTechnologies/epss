96052e698fdb7ca9a64d81bff5d4f04d54b9a7c5
Firose Hussain <github@firumon.com>
2019-06-22 13:22:20 +0530

    Name will be displayed in title bar

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,12 +1,12 @@
 <!--suppress ALL -->
 <template>
-    <App title="User Name">
-<!--        <UserStoresInfoWithIcon width="90%"></UserStoresInfoWithIcon>-->
-<!--        <UserAreasInfoWithIcon width="90%"></UserAreasInfoWithIcon>-->
-<!--        <TextTitleSub width="90%" class="m-t-20 cp">Outstandings</TextTitleSub>-->
-<!--        <UserCustomerTotalOutstandingMetric width="90%"></UserCustomerTotalOutstandingMetric>-->
-<!--        <TextTitleSub width="90%" class="m-t-20 cp">Sales Orders</TextTitleSub>-->
-<!--        <SalesOrderPendingMetric width="90%" class="m-b-10"></SalesOrderPendingMetric>-->
+    <App :title="name">
+        <UserStoresInfoWithIcon width="90%"></UserStoresInfoWithIcon>
+        <UserAreasInfoWithIcon width="90%"></UserAreasInfoWithIcon>
+        <TextTitleSub width="90%" class="m-t-20 cp">Outstandings</TextTitleSub>
+        <UserCustomerTotalOutstandingMetric width="90%"></UserCustomerTotalOutstandingMetric>
+        <TextTitleSub width="90%" class="m-t-20 cp">Sales Orders</TextTitleSub>
+        <SalesOrderPendingMetric width="90%" class="m-b-10"></SalesOrderPendingMetric>
         <template v-for="(items,caption,sidx) in menus">
             <TextTitleSub class="m-t-12 m-b-8 m-l-2">{{ caption }}</TextTitleSub>
             <GridMenuRow :menus="items"></GridMenuRow>
@@ -15,13 +15,11 @@
 </template>
 
 <script>
-    import { mapGetters } from 'vuex'
-    import TextHeading from "./typography/TextHeading";
+    import { mapState,mapGetters } from 'vuex'
     export default {
         name: "Home",
-        components: {TextHeading},
         computed: {
-            ...mapGetters('Menu', ['menus']),
+            ...mapGetters('Menu', ['menus']),...mapState('User', ['name','email']),
         }
     }
 </script>
