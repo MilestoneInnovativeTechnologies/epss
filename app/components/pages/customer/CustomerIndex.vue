e4c8e84e4b6b736aaf41e8db560aba9faab87c8a
Firose Hussain <github@firumon.com>
2019-06-22 21:16:12 +0530

    Customer listing

--- app/components/pages/customer/CustomerIndex.vue
+++ app/components/pages/customer/CustomerIndex.vue
@@ -1,23 +1,29 @@
 <template>
     <App title="Customers">
-
+        <AppList :source="list" :layout="layout" detail="customer/CustomerDetail"></AppList>
     </App>
 </template>
 
 <script>
-    import { mapGetters } from 'vuex'
+    import { mapState,mapActions } from 'vuex'
+    import {user_assigned_area_customers} from "../../../assets/scripts/queries";
 
     export default {
         name: "CustomerIndex",
         props: ['id'],
         data(){ return {
-
+            key: 'list', path: '',
+            layout: { Name:'name', Phone:'phone' }
         } },
-        computed:{
-            ...mapGetters('Customer',[]),
+        computed: {
+            ...mapState('User',['id']),...mapState('Customer',['list']),
         },
         methods: {
-
+            ...mapActions('Customer',['_stockIfNot'])
         },
+        created() {
+            let query = sql.format(user_assigned_area_customers,this.id);
+            this._stockIfNot({ query:query,path:this.path,key:this.key })
+        }
     }
 </script>
