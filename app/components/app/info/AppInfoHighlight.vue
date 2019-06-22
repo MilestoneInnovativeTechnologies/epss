4c4cf43bfa6dd2d76e3aa98d8bb03fd2a3d42a2b
Firose Hussain <github@firumon.com>
2019-06-22 16:00:53 +0530

    Highlight and With label components added

---
+++ app/components/app/info/AppInfoHighlight.vue
@@ -0,0 +1,14 @@
+<template>
+    <FlexBoxLayout flexDirection="column">
+        <TextDisplay class="text-center" :text="mText"></TextDisplay>
+        <TextTitleSubSmall class="text-center cp">{{ title }}</TextTitleSubSmall>
+    </FlexBoxLayout>
+</template>
+
+<script>
+    export default {
+        name: "AppInfoHighlight",
+        props: ['title'],
+        mixins: [require('./../../../assets/scripts/mixins/typography').default],
+    }
+</script>
---
+++ app/components/app/info/AppInfoWithLabel.vue
@@ -0,0 +1,14 @@
+<template>
+    <StackLayout>
+        <TextHeadingRowSub>{{ title }}</TextHeadingRowSub>
+        <TextBold :text="mText"></TextBold>
+    </StackLayout>
+</template>
+
+<script>
+    export default {
+        name: "AppInfoWithLabel",
+        props: ['title'],
+        mixins: [require('./../../../assets/scripts/mixins/typography').default],
+    }
+</script>
