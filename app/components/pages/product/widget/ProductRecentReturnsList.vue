7ed977f4e81bf008128c68664f5108d601635c55
Firose Hussain <github@firumon.com>
2019-06-22 10:29:40 +0530

    Transaction list made use of

--- app/components/pages/product/widget/ProductRecentReturnsList.vue
+++ app/components/pages/product/widget/ProductRecentReturnsList.vue
@@ -1,28 +1,20 @@
 <template>
-    <AppList title="Recent Returns" :limit="limit" :source="recentReturnsSource" :layout="layout" :links="{ customer:['customer/CustomerDetail',{ id:'cid' }] }" detail="pages/sales/SaleReturnDetail" :props="{ id:'_ref' }"></AppList>
+    <RecentTransactionList trType="SR" title="Recent Sales Returns" :path="path" :fields="fields" :condition="condition" :limit="limit" :layout="layout" :cast="cast" :links="{ customer:['customer/CustomerDetail',{ id:'cid' }] }" detail="sales/SaleReturnDetail"></RecentTransactionList>
 </template>
 
 <script>
-    import { mapGetters,mapActions,mapState } from 'vuex'
-
     export default {
         name: "ProductRecentReturnsList",
         props: ['id','limit'],
         data(){ return {
+            fields: ['id','docno','cid','customer','date','quantity','nature'],
             layoutTemplate:['docno','customer','date','quantity','nature'],
-            queryTemplate: `SELECT TR.docno,TR.customer cid, TR.date,CS.name customer,SPT.quantity,PTN.name nature,TR._ref FROM transactions TR,users CS, transaction_details TD, store_product_transactions SPT, product_transaction_natures PTN WHERE TR.customer = CS.id AND TR._ref = TD.\`transaction\` AND TD.spt = SPT._ref AND SPT.nature = PTN.id AND SPT.store IN ("#STORES#") AND TR.\`customer\` IN ("#CUSTOMERS#") AND SPT.product = "${this.id}" AND TR.fncode LIKE "SR%" ORDER BY TR.\`date\` DESC`,
+            cast:{ date:'docdate',quantity:'quantity' },
         }},
         computed: {
-            ...mapGetters('User',{ userStores:'stores',userCustomers:'customers' }), ...mapState('Product',['transactions']),
-            query(){ return _.replace(_.replace(this.queryTemplate,/#STORES#/g,this.userStores.join('","')),/#CUSTOMERS#/,this.userCustomers.join('","')) },
-            recentReturnsSource(){ return _.map(this.transactions[this.id],(item) => { return { ...item,date:(moment(item.date).format(__.DOCDATE_FORMAT)),quantity:_.round(item.quantity,__.QUANTITY_DECIMAL) } }) },
+            path(){ return 'PS' + this.id },
+            condition(){ return _.zipObject(['pid'],[this.id]) },
             layout(){ return _.mapKeys(this.layoutTemplate,(item) => _.capitalize(item))},
-        },
-        methods: {
-            ...mapActions('Product',['_stock']),
-        },
-        created() {
-            this._stock({ query:this.query,key:'transactions',path:this.id })
         }
     }
 </script>
--- app/components/pages/product/widget/ProductRecentSalesList.vue
+++ app/components/pages/product/widget/ProductRecentSalesList.vue
@@ -1,28 +1,20 @@
 <template>
-    <AppList title="Recent Sales" :limit="limit" :source="recentSalesSource" :layout="layout" :links="{ customer:['customer/CustomerDetail',{ id:'cid' }] }" detail="pages/sales/SaleDetail" :props="{ id:'_ref' }"></AppList>
+    <RecentTransactionList trType="SL" title="Recent Sales" :path="path" :fields="fields" :condition="condition" :limit="limit" :layout="layout" :cast="cast" :links="{ customer:['customer/CustomerDetail',{ id:'cid' }] }" detail="sales/SaleDetail"></RecentTransactionList>
 </template>
 
 <script>
-    import { mapGetters,mapActions,mapState } from 'vuex'
-
     export default {
         name: "ProductRecentSalesList",
         props: ['id','limit'],
         data(){ return {
             layoutTemplate:['docno','customer','date','quantity'],
-            queryTemplate: `SELECT TR.docno,TR.customer cid, TR.date,CS.name customer,SPT.quantity,TR._ref FROM transactions TR,users CS, transaction_details TD, store_product_transactions SPT WHERE TR.customer = CS.id AND TR._ref = TD.\`transaction\` AND TD.spt = SPT._ref AND SPT.store IN ("#STORES#") AND TR.\`customer\` IN ("#CUSTOMERS#") AND SPT.product = "${this.id}" AND TR.fncode LIKE "SL%" ORDER BY TR.\`date\` DESC`,
+            fields: ['id','docno','cid','customer','date','quantity'],
+            cast:{ date:'docdate',quantity:'quantity' }
         }},
         computed: {
-            ...mapGetters('User',{ userStores:'stores',userCustomers:'customers' }), ...mapState('Product',['transactions']),
-            query(){ return _.replace(_.replace(this.queryTemplate,/#STORES#/g,this.userStores.join('","')),/#CUSTOMERS#/,this.userCustomers.join('","')) },
-            recentSalesSource(){ return _.map(this.transactions[this.id],(item) => { return { ...item,date:(moment(item.date).format(__.DOCDATE_FORMAT)),quantity:_.round(item.quantity,__.QUANTITY_DECIMAL) } }) },
             layout(){ return _.mapKeys(this.layoutTemplate,(item) => _.capitalize(item))},
-        },
-        methods: {
-            ...mapActions('Product',['_stock']),
-        },
-        created() {
-            this._stock({ query:this.query,key:'transactions',path:this.id })
+            path(){ return 'PR' + this.id },
+            condition(){ return _.zipObject(['pid'],[this.id]) }
         }
     }
 </script>
