383ace582d7d5e383fabcbd646a43eab5509e3a8
Firose Hussain <github@firumon.com>
2019-06-23 10:06:34 +0530

    Created SOIndex

--- app/components/pages/sales/SalesOrderIndex.vue
+++ app/components/pages/sales/SalesOrderIndex.vue
@@ -1,13 +1,22 @@
 <template>
-    <App title="Sales Order"></App>
+    <App title="Sales Order">
+        <AppList :source="source" :layout="layout" :cast="cast" :links="links" detail="sales/SalesOrderDetail"></AppList>
+    </App>
 </template>
 
 <script>
+    import { mapState } from 'vuex';
+
     export default {
-        name: "SalesOrderIndex"
+        name: "SalesOrderIndex",
+        data(){ return {
+            layout: { Customer:'customer',Date:'date',Status:'progress' },
+            cast: { date:'docdate' },
+            links: { customer:['customer/CustomerDetail',{ id:'cid' }] }
+        }},
+        computed: {
+            ...mapState('SalesOrder',['list']),
+            source(){ return __.cast(this.list,this.cast) }
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
