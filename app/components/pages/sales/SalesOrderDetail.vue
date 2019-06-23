4d3e2cc783908e9e498512a3a594e63c41569763
Firose Hussain <github@firumon.com>
2019-06-23 10:08:29 +0530

    SO Detail created

---
+++ app/components/pages/sales/SalesOrderDetail.vue
@@ -0,0 +1,12 @@
+<template>
+    <App title="Sales Order Detail">
+        <DevMsg></DevMsg>
+    </App>
+</template>
+
+<script>
+    export default {
+        name: "SalesOrderDetail",
+        props: ['id'],
+    }
+</script>
