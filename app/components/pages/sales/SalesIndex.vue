d7e9163416e592d5af93c4f4faf98ac8ba37e5a5
Firose Hussain <github@firumon.com>
2019-06-23 12:38:36 +0530

    Created

--- app/components/pages/sales/SalesIndex.vue
+++ app/components/pages/sales/SalesIndex.vue
@@ -1,13 +1,33 @@
 <template>
-    <App title="Sales"></App>
+    <App title="Sales">
+        <AppList :source="source" :layout="layout" :links="links" detail="sales/SaleDetail" :title="title"></AppList>
+    </App>
 </template>
 
 <script>
+    import { mapActions,mapState } from 'vuex';
+    import {TransactionQueryBuilder} from "../../../assets/scripts/services/transactionquery";
+
     export default {
-        name: "SalesIndex"
+        name: "SalesIndex",
+        data(){ return {
+            title: 'Recent Sales',
+            fields: ['docno','date','customer','cid','id'],
+            layout: { Customer:'customer',Date:'date','DOC NO':'docno' },
+            cast: { date:'docdate' },
+            limit: 10, max: 30, root:'transactions',path:'all',
+            links: { customer:['customer/CustomerDetail',{ id:'cid' }] }
+        }},
+        computed: {
+            ...mapState('Sales',['transactions']),
+            source(){ return __.cast(this[this.root][this.path],this.cast) }
+        },
+        methods: {
+            ...mapActions('Sales',['_stockIfNot'])
+        },
+        created() {
+            let query = new TransactionQueryBuilder('SL').fields(this.fields).max(this.max).query();
+            this._stockIfNot({ query,key:this.root,path:this.path })
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
