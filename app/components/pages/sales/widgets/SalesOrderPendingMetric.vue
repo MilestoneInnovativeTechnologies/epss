132cff1a2d88c783299118e3e435c07932e7e582
Firose Hussain <github@firumon.com>
2019-06-23 09:22:11 +0530

    Rearranged to use own state data

--- app/components/pages/sales/widgets/SalesOrderPendingMetric.vue
+++ app/components/pages/sales/widgets/SalesOrderPendingMetric.vue
@@ -3,23 +3,19 @@
 </template>
 
 <script>
-    import { mapGetters } from 'vuex';
+    import { mapState } from 'vuex';
 
     export default {
         name: "SalesOrderPendingMetric",
         data(){ return {
-            metricTemplate: ['pending','partial'],
+            metricTemplate: ['Incomplete','Partial'],
             metricIcons: [],
             coloured: true, size:30
         }},
         computed: {
-            ...mapGetters('SalesOrder',{ SOPending:'pending',SOPartial:'partial'}),
-            pending(){ return _.size(this.SOPending) }, partial(){ return _.size(this.SOPartial) },
-            items(){ let vm = this; return _.map(this.metricTemplate,(item) => _.zipObject(['text','title','coloured','size'],[vm[item],_.upperCase(item),vm.coloured,vm.size])) }
+            ...mapState('SalesOrder',['list']),
+            pCount(){ return _.countBy(this.list,'progress') },
+            items(){ let vm = this; return _.map(this.metricTemplate,(item) => _.zipObject(['text','title','coloured','size'],[_.toSafeInteger(vm.pCount[item]),_.upperCase(item),vm.coloured,vm.size])) }
         },
     }
 </script>
-
-<style scoped>
-
-</style>
