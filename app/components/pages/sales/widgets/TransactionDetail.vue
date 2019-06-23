c62f52a553936dcfeff1b8598c8f6661a152ff64
Firose Hussain <github@firumon.com>
2019-06-23 06:51:29 +0530

    No details will be show for empty records

--- app/components/pages/sales/widgets/TransactionDetail.vue
+++ app/components/pages/sales/widgets/TransactionDetail.vue
@@ -1,16 +1,19 @@
 <template>
-    <StackLayout>
+    <StackLayout v-if="transaction">
         <TextTitle>{{ transaction.docno }}</TextTitle>
         <TextTitleSub class="m-b-15">{{ transaction.customer }}</TextTitleSub>
         <AppInfoWide title="date">{{ docdate(transaction.date) }}</AppInfoWide>
         <AppInfoWide title="executive" class="m-b-15">{{ transaction.executive }}</AppInfoWide>
-        <AppList :source="source" :layout="productListLayout" title="Products"></AppList>
+        <AppList :source="source" :layout="layout" title="Products"></AppList>
         <GridLayout rows="auto" columns="*,*,*" class="m-t-15">
-            <AppInfoWithLabel row="0" col="0" title="TAX">{{ (sum('tax')) }}</AppInfoWithLabel>
-            <AppInfoWithLabel row="0" col="1" title="DISCOUNT">{{ (sum('discount')) }}</AppInfoWithLabel>
-            <AppInfoWithLabel row="0" col="2" title="AMOUNT">{{ (sum('total')) }}</AppInfoWithLabel>
+            <AppInfoWithLabel row="0" col="0" title="TAX">{{ round(sum('tax')) }}</AppInfoWithLabel>
+            <AppInfoWithLabel row="0" col="1" title="DISCOUNT">{{ round(sum('discount')) }}</AppInfoWithLabel>
+            <AppInfoWithLabel row="0" col="2" title="AMOUNT">{{ round(sum('total')) }}</AppInfoWithLabel>
         </GridLayout>
-        <AppInfoHighlight title="payable amount" class="m-t-20">{{ (sum('total')) }}</AppInfoHighlight>
+        <AppInfoHighlight title="payable amount" class="m-t-20">{{ round(sum('total')) }}</AppInfoHighlight>
+    </StackLayout>
+    <StackLayout v-else>
+        <TextHeading width="100%" class="text-center">No details</TextHeading>
     </StackLayout>
 </template>
 
@@ -26,17 +29,17 @@
         computed: {
             ...mapState('Transaction',['detail']),
             transactionDetail(){ return this.detail[this.id] }, transaction(){ return this.transactionDetail[0] },
-            productListLayout(){ return _.mapKeys(this.layout,(item) => _.capitalize(item)) },
-            source(){ if(_.isNil(this.cast)) return this.transactionDetail; let cast = this.cast, castKeys = _.keys(cast); return _.map(this.transactionDetail,(detail) => _.mapValues(detail,(value, name) => _.includes(castKeys,name) ? __[cast[name]](value) : value) ) },
+            source(){ return _.isNil(this.cast) ? this.transactionDetail : __.cast(this.transactionDetail,this.cast); },
             sum(){ return (field) => _.sum(_.map(this.transactionDetail,(item) => _.toNumber(item[field]))) },
-            docdate(){ return (date) => __.docdate(date) }, round(){ return (num) => (__.amount(num)).toFixed(__.AMOUNT_DECIMAL) }
         },
         methods: {
-            ...mapActions('Transaction',['_stockIfNot'])
+            ...mapActions('Transaction',['_stockIfNot']),
+            docdate(date){ return __.docdate(date) },
+            round(number){ return __.amount(number).toFixed(__.AMOUNT_DECIMAL) },
         },
         created() {
             let query = new TransactionQueryBuilder(this.trType).fields(this.fields).where({ id:this.id }).query();
-            this._stockIfNot({ query,key:'detail',path:this.id,on:this.cacheOn })
+            this._stockIfNot({ query:this.query,key:'detail',path:this.id,on:this.cacheOn })
         }
     }
 </script>
