e23a712399cf6ec40a1326f979a98cac3f6c5866
Firose Hussain <github@firumon.com>
2019-06-23 10:08:16 +0530

    Development message component

---
+++ app/components/DevMsg.vue
@@ -0,0 +1,9 @@
+<template>
+    <TextTitle with="100%" class="text-center">Window under development</TextTitle>
+</template>
+
+<script>
+    export default {
+        name: "DevMsg"
+    }
+</script>
