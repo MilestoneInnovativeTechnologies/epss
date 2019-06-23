f173c4fcb8d596c96fab31c05196a12f22f2e760
Firose Hussain <github@firumon.com>
2019-06-23 08:47:05 +0530

    Updated to take data from customer

--- app/components/pages/user/widgets/UserCustomerTotalOutstandingMetric.vue
+++ app/components/pages/user/widgets/UserCustomerTotalOutstandingMetric.vue
@@ -8,23 +8,18 @@
     export default {
         name: "UserCustomerTotalOutstandingMetric",
         data(){ return {
-            queryTemplate: `SELECT SUM(outstanding_normal) normal, SUM(outstanding_overdue) overdue, SUM(outstanding_critical) critical FROM users WHERE id IN ("#USERS#")`,
             metricTemplate: ['normal','overdue','critical'],
             metricIcons: ['attach_money','error_outline','warning'],
             size:25, coloured: true,
             itemKeys: ['coloured','size','icon','text','title']
         } },
         computed: {
-            ...mapGetters('User',['customers']), ...mapState('Customer',['outstanding']),
-            query(){ return _.replace(this.queryTemplate,/#USERS#/g,this.customers.join('","'))},
-            itemText(){ return this.outstanding['all'][0] },
+            ...mapState('Customer',['list']),
+            itemText(){ return _.mapValues(_.zipObject(this.metricTemplate,this.metricTemplate),(type) => this.getSum('outstanding_'+type) ) },
             items(){ let vm = this; return _.map(this.metricTemplate,(title,idx) => _.zipObject(vm.itemKeys,[vm.coloured,vm.size,vm.metricIcons[idx],__.amount(vm.itemText[title]),title])) }
         },
         methods: {
-            ...mapActions('Customer',['_stock']),
-        },
-        created() {
-            this._stock({ query:this.query,key:'outstanding',path:'all' });
+            getSum(key){ return __.amount(_.sum(_.map(this.list,(item) => _.toNumber(item[key])))) }
         }
     }
 </script>
