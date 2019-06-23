330c5247eeade84fd495eb1b84556f28f1f14c68
Firose Hussain <github@firumon.com>
2019-06-23 09:22:54 +0530

    No getters using as of now

--- app/assets/scripts/vuex/modules/salesorders/getters.js
+++ app/assets/scripts/vuex/modules/salesorders/getters.js
@@ -1,9 +1,1 @@
-export function incomplete(state,{ _tableDataFilter }) {
-    return _tableDataFilter('sales_order','progress','Incomplete')
-}
-export function partial(state,{ _tableDataFilter }) {
-    return _tableDataFilter('sales_order','progress','Partial')
-}
-export function pending(state,{ Incomplete,Partial }) {
-    return _.concat(Incomplete,Partial)
-}
+export function none(state){ }
