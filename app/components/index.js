efec53362c7f660afe63a971db747a08f3eb42e2
Firose Hussain <github@firumon.com>
2019-06-23 19:43:48 +0530

    RadDataForm plugin added

--- app/components/index.js
+++ app/components/index.js
@@ -1,13 +1,13 @@
 import Vue from 'nativescript-vue';
 
-// import RadDataForm from 'nativescript-ui-dataform/vue';
+import RadDataForm from 'nativescript-ui-dataform/vue';
 
 
 
 
 
 
-// Vue.use(RadDataForm);
+Vue.use(RadDataForm);
 
 const files = require.context('./', true, /\.vue$/i);
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
