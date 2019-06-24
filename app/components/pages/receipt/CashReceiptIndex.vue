a255ee65cd1b14f2c7a9e81c0ea3783468f4bb9f
Firose Hussain <github@firumon.com>
2019-06-24 12:52:22 +0530

    Under development

--- app/components/pages/receipt/CashReceiptIndex.vue
+++ app/components/pages/receipt/CashReceiptIndex.vue
@@ -1,13 +1,26 @@
 <template>
-    <App title="Cash Receipt"></App>
+    <App title="Cash Receipt">
+<!--        <DevMsg></DevMsg>-->
+        <AppForm :fields="fields" title="TEST FORM" :values="values" @date="customerChanged"></AppForm>
+    </App>
 </template>
 
 <script>
+    const feMX = require("../../../assets/scripts/mixins/formelement");
+
     export default {
-        name: "CashReceiptIndex"
+        name: "CashReceiptIndex",
+        data() { return {
+            formFields: { customer:'Customer',fiscal:'Fiscal',date:'DatePicker' },
+            value: {},
+        }},
+        mixins: [feMX.customer,feMX.fiscalyear,feMX.dataepicker],
+        computed: {
+            fields(){ return this.formFieldsToFields() },
+        },
+        methods: {
+            customerChanged(v){ console.log('CV',v); },
+            formFieldsToFields(){ return (_.isEmpty(this.formFields)) ? {} : _.mapValues(this.formFields,(mxn,name) => { return { ...(this['feField'+mxn]),name }}) }
+        },
     }
 </script>
-
-<style scoped>
-
-</style>
