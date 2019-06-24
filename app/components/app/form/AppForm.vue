4897ed3fb991d7933aa5b977a7785a8b6227fade
Firose Hussain <github@firumon.com>
2019-06-24 12:48:18 +0530

    AppFrom updated

--- app/components/app/form/AppForm.vue
+++ app/components/app/form/AppForm.vue
@@ -11,22 +11,20 @@
     export default {
         name: "AppForm",
         components: {TextTitleSub},
-        props: ['fields', 'values', 'action', 'title'],
+        props: ['fields', 'value', 'action', 'title'],
         data() {
             return {
                 commit: 'Immediate', validate: 'Immediate', read: false,
-                morph: {label: 'displayName', type: 'editor'},
+                morph: {label: 'displayName', type: 'editor', values: 'valuesProvider'},
                 valProTypes: ['Picker', 'SegmentedEditor', 'List', 'AutoCompleteInline'],
-                final: {},
+                source:{}, final: {},
             }
         },
         computed: {
-            source() { return _.mapValues(this.fields, (fldObj, name) => this.getInitValue(name)); },
             propertyAnnotations() {
-                let fields = this.fields, values = this.values, Annotations = [];
+                let fields = this.fields, Annotations = [];
                 _.forEach(fields, (Obj, name) => {
-                    let value = _.has(values, name) ? values[name] : null;
-                    let idx = Annotations.push(this.getPropertyAnnotation(name, Obj, value)) - 1;
+                    let idx = Annotations.push(this.getPropertyAnnotation(name, Obj)) - 1;
                     Annotations[idx]['index'] = idx;
                 });
                 return Annotations;
@@ -38,30 +36,28 @@
             }
         },
         methods: {
-            getPropertyAnnotation(name, Obj, value) {
-                let morph = this.morph, Annotation = {name}, valProTypes = this.valProTypes;
+            getPropertyAnnotation(name, Obj) {
+                let morph = this.morph, Annotation = {name};
                 Annotation = _.reduce(Obj, function (Annotation, value, key) {
                     Annotation[_.has(morph, key) ? morph[key] : key] = value;
                     return Annotation;
                 }, Annotation);
-                console.log(name,_.includes(valProTypes, name),!!value);
-                if (!!value && _.includes(valProTypes, Obj.type)) Annotation['valuesProvider'] = value;
                 return Annotation;
             },
             getInitValue(name, values) {
-                values = values || this.values;
+                values = values || this.value;
                 if (!values || _.isEmpty(values) || !_.has(values, name)) return '';
                 return (_.isArray(values[name])) ? 0 : values[name];
             },
             submitForm(){ this.$refs.radDataForm.nativeView.validateAndCommitAll().then(result => this.$emit('submit',this.final)) },
             formPropsCommitted(data) {
                 let field = data.propertyName, editedObj = JSON.parse(data.object.editedObject), value = _.get(editedObj,field);
-                if(this.fields[field].type === 'Picker') value = _.indexOf(this.values[field],value);
+                // if(this.fields[field].type === 'Picker') value = _.indexOf(this.value[field],value);
                 this.final = Object.assign({},this.final, _.fromPairs([[field,value]])); this.$emit(field,value);
             }
         },
-        watch: {
-            values: { deep: true, immediate: true, handler(data) { _.forEach(this.fields, (nameObj, name) => this.final[name] = this.getInitValue(name, data)); } }
+        created(){
+            _.forEach(this.fields, (nameObj, name) => { let value = this.getInitValue(name);  this.source[name] = value; this.final[name] = value; });
         }
     }
 </script>
