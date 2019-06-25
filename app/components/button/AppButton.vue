c7fb2bf1c6333996fe34e43928199c778244b608
Firose Hussain <github@firumon.com>
2019-06-25 18:56:48 +0530

    Component for button

---
+++ app/components/button/AppButton.vue
@@ -0,0 +1,10 @@
+<template>
+    <Button class="bcp fsb" :text="mText"></Button>
+</template>
+
+<script>
+    export default {
+        name: "AppButton",
+        mixins: [require('./../../assets/scripts/mixins/typography').default],
+    }
+</script>
