5cd6d99d626027641524090c1456488c5fa3c301
Firose Hussain <github@firumon.com>
2019-06-25 09:26:05 +0530

    Added products ant natures fetch queries

--- app/assets/scripts/queries.js
+++ app/assets/scripts/queries.js
@@ -4,3 +4,5 @@ export const user_assigned_area_customers = `SELECT * FROM users WHERE EXISTS ( 
 export const user_assigned_customer_sales_orders = `SELECT sales_order.*,sales_order._ref id,customer.name customer,customer.id cid FROM sales_order,users customer WHERE progress <> 'Completed' AND sales_order.customer = customer.id AND EXISTS(SELECT * FROM area_users WHERE area_users.user = sales_order.customer AND EXISTS(SELECT * FROM user_store_area WHERE user_store_area.area = area_users.area AND user_store_area.user = ? ))`;
 export const sales_order_items_of_a_sales_order = `SELECT SOI.product pid,SOI.rate,SOI.quantity,SOI._ref,P.name AS product FROM sales_order_items SOI,products P WHERE SOI.product = P.id AND SOI.so = ?`
 export const fetch_fiscal_year_details = `SELECT * FROM fiscalyearmaster`;
+export const fetch_all_products = `SELECT * FROM products`;
+export const fetch_all_transaction_natures = `SELECT * FROM product_transaction_natures`;
