f3460fbfc6d747377a5b54765e1465b434a220df
Firose Hussain <github@firumon.com>
2019-06-25 09:40:52 +0530

    Key added for v-for

--- app/components/app/metric/AppMetric.vue
+++ app/components/app/metric/AppMetric.vue
@@ -1,6 +1,6 @@
 <template>
     <GridLayout rows="*" :columns="columns" >
-        <MetricItem row="0" v-for="(item,colNo) in items" :col="colNo" v-bind="item"></MetricItem>
+        <MetricItem row="0" v-for="(item,colNo) in items" :col="colNo" v-bind="item" :key="['metric',unique,'col',colNo].join('-')"></MetricItem>
     </GridLayout>
 </template>
 
@@ -11,6 +11,7 @@
             items: { type:Array,default:()=>[{ icon:'receipt',text:'display',title:'title'}] }
         },
         computed: {
+            unique(){ return new Date().getTime() },
             itemsCount(){ return this.items.length },
             columns(){ return _.fill(Array(this.itemsCount),'*').join(',') }
         }
