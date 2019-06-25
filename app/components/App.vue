dddcd00d94d4821bd1b96c785351ddf0209798d8
Firose Hussain <github@firumon.com>
2019-06-25 18:54:23 +0530

    Layout changed to accommodate fixed button

--- app/components/App.vue
+++ app/components/App.vue
@@ -2,17 +2,28 @@
 <template>
     <Page actionBarHidden="false" backgroundSpanUnderStatusBar="true">
         <CustomActionBar :title="title"></CustomActionBar>
-        <ScrollView>
-            <StackLayout width="95%" class="m-t-10">
-                <slot></slot>
-            </StackLayout>
-        </ScrollView>
+        <GridLayout :rows="rows">
+            <ScrollView row="0"><StackLayout class="p-t-10" width="95%"><slot></slot></StackLayout></ScrollView>
+            <StackLayout row="1" orientation="horizontal"><AppButton class="c-white" :width="width" v-for="(btn,idx) in actions" @tap.native="$emit(eventName(btn))" :key="['app',unique,'btn',idx].join('-')">{{ btn }}</AppButton></StackLayout>
+        </GridLayout>
     </Page>
 </template>
 
 <script>
     export default {
         name: 'App',
-        props: ['title'],
+        props: ['title','action'],
+        data(){ return {
+            fixHeight: 60
+        }},
+        computed: {
+            unique(){ return new Date().getTime() },
+            rows(){ let rows = ['*']; rows.push(_.toSafeInteger(!!this.action)*this.fixHeight); return rows.join(','); },
+            actions(){ return _.concat(this.action); },
+            width(){ return _.toSafeInteger(100/this.actions.length)+'%' }
+        },
+        methods: {
+            eventName(name){ return _.kebabCase(name) }
+        }
     }
 </script>
