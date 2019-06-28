eaf513fa7ac41fe9fa059f15048a6314bb1547ec
Firose Hussain <github@firumon.com>
2019-06-28 21:30:05 +0530

    Import corrected

--- app/assets/scripts/vuex/store.js
+++ app/assets/scripts/vuex/store.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import state from './state';
-import getters from './getters';
+import * as getters from './getters';
 import mutations from './mutations';
 import * as actions from './actions';
 
