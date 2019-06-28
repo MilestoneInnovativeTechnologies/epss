9d361ec301e1792fd90cde17ba4fbdb2455a93aa
Firose Hussain <github@firumon.com>
2019-06-28 18:57:42 +0530

    modified

--- app/components/Debug.vue
+++ app/components/Debug.vue
@@ -1,13 +1,11 @@
 <!--suppress ALL -->
 <template>
     <App title="DEBUG">
-        <AppList v-if="tblInfoSource.length" :source="tblInfoSource" :layout="tblInfolayout" title="Table Informations"></AppList>
-        <Button v-else text="Fetch Table Informations" @tap="fTI" />
         <AppList v-if="qLogSource.length" :source="qLogSource" :layout="qLogLayout" title="Query Log"></AppList>
         <Button v-else text="Query Log" @tap="fQL" />
-        <ListPicker :items="tables" v-model="selectedTableIndex"></ListPicker>
+        <AppForm :fields="tablePicker" @picker="selectedTableIndex = $event"></AppForm>
         <Button text="Get Table Data" @tap="gTD" />
-        <AppList :source="selectedTableData" :title="tables[selectedTableIndex]"></AppList>
+        <AppList :source="selectedTableInfoData" :layout="tblInfolayout" :title="tables[selectedTableIndex]"></AppList>
     </App>
 </template>
 
@@ -17,24 +15,25 @@
 
     export default {
         name: "Debug",
+        mixins: [feMX.picker,feMX.common],
         data() {
             return {
                 tblInfoSource:[],
-                tblInfoLays: ['table','type','up','down','sync','next','create','update'],
+                tblInfoLays: ['table','records'],
                 qLogSource:[],  qLogLayout:{ Query:'qry' },
-                tables:['setup','area_users','areas','fiscalyearmaster','functiondetails','pricelist_header','pricelist','product_transaction_natures','product_transaction_types','productgroups','products','sales_order','sales_order_items','settings','stores','store_product_transactions','transactions','transaction_details','stock_transfer','user_settings','user_store_area','users'],
+                tables:['setup','area_users','areas','fiscalyearmaster','functiondetails','pricelist_header','pricelist','product_transaction_natures','product_transaction_types','productgroups','products','sales_order','sales_order_items','settings','stores','store_product_transactions','transactions','transaction_details','stock_transfer','user_settings','user_store_area','users','receipts','fn_reserves'],
                 selectedTableIndex:0,
-                selectedTableData:[],
+                selectedTableInfoData:[],
             }
         },
         computed: {
             ...mapState('Sync',['queue','queue_index','processing','url','time','user','client']),
-            tblInfolayout(){ return _.mapKeys(this.tblInfoLays,(l) => _.capitalize(l) ) }
+            tblInfolayout(){ return _.mapKeys(this.tblInfoLays,(l) => _.capitalize(l) ) },
+            tablePicker(){ return { picker: {...(this.feFieldPicker),name:'picker',values:this.tables }} }
         },
         methods: {
             fQL(){ let qLog = DB.log(); this.qLogSource = _.map(qLog,(qry) => { return { qry } }) },
-            fTI(){ DB.get(table_information_db_table_name,null,function (vm) { vm.tblInfoSource = this.result; },this) },
-            gTD(){ DB.get(this.tables[this.selectedTableIndex],null,function(vm){ vm.selectedTableData.splice(0); Array.prototype.push.apply(vm.selectedTableData,this.result) },this) },
+            gTD(){ DB.get(this.tables[this.selectedTableIndex],null,function(vm){ vm.selectedTableInfoData.push({ table:this.tbl, records:this.result.length }) },this) },
         },
     }
 </script>
