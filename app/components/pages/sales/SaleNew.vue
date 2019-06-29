a5558768dc2f40062530a2180517f291be56b112
Firose Hussain <github@firumon.com>
2019-06-29 10:07:01 +0530

    New Sale component created

--- app/components/pages/sales/SaleNew.vue
+++ app/components/pages/sales/SaleNew.vue
@@ -5,22 +5,27 @@
 </template>
 
 <script>
+    import { mapGetters } from 'vuex';
     const feMX = require('./../../../assets/scripts/mixins/formelement');
 
     export default {
         name: "SaleNew",
-        mixins: [feMX.common,feMX.customer,feMX.fiscal,feMX.datepicker,feMX.payment,feMX.salesorder,feMX.nature],
+        mixins: [feMX.common,feMX.customer,feMX.store,feMX.salestype,feMX.fiscal,feMX.datepicker,feMX.payment,feMX.nature],
         data(){ return {
-            fieldLayout: { customer:'Customer',fiscal:'Fiscal',date:'DatePicker',payment:'Payment',salesorder:'SalesOrder' },
-            values: { date:moment().format('YYYY-MM-DD') },
+            fieldLayout: { customer:'Customer',store:'Store',type:'SalesType',fiscal:'Fiscal',date:'DatePicker',payment:'Payment' },
             final: {}
         }},
         computed: {
+            ...mapGetters('Sales',['docno']),...mapGetters('Fiscal',['_tableDataItem']),...mapGetters(['date']),
+            values(){ return { date:this.date() } }
         },
         methods: {
             setSelectedData(data){ this.final = Object.assign({},this.final,data); },
             proceed(){
-                this.$navigateTo(require('./SaleNewItems').default,{ props:{'master':this.final} })
+                let { store,fiscal,type,customer,date,payment } = this.final, docno = this.docno(store,fiscal,type);
+                let fycode = _.get(this._tableDataItem('fiscalyearmaster',fiscal),'code') || '2019';
+                let master = { docno,customer,date,fycode,fncode:type,payment_type:payment };
+                this.$navigateTo(require('./SaleNewItems').default,{ props:{ master } })
             }
         }
     }
