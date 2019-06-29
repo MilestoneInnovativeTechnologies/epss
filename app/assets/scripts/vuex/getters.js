668935f86ac51900e66b1444644fcb9c72929359
Firose Hussain <github@firumon.com>
2019-06-29 10:02:25 +0530

    Global getters for document generating properties

--- app/assets/scripts/vuex/getters.js
+++ app/assets/scripts/vuex/getters.js
@@ -1,1 +1,16 @@
 export function _ref(state){ return () => ['U',state['User'].id,'T',moment().format('x')].join('') }
+export function date(){ return () => moment().format('YYYY-MM-DD') }
+export function datetime(){ return () => moment().format('YYYY-MM-DD HH:mm:ss') }
+export function docno(){
+    return (format,object) => {
+        let replaces = ['CMP','BR','FN','FY','AI'];
+        return _.reduce(replaces,(format,key) => object[key] ? format.replace(`[${key}]`,object[key]) : format,format);
+    }
+}
+export function abbreviations(state,getters){
+    return (store,fyid) => {
+        let str = _.get(getters['Stores/_tableDataById']('stores'),store);
+        let FY = _.get(getters['Fiscal/_tableDataItem']('fiscalyearmaster',fyid),'abr');
+        return _.zipObject(['FY','BR','CMP'],[FY,str.br_abr,str.co_abr]);
+    }
+}
