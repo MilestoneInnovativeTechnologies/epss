df7644d65532147f622a6e3ac83d066a1292f4fb
Firose Hussain <github@firumon.com>
2019-06-29 22:33:51 +0530

    SPT Sale attributes

--- app/assets/scripts/vuex/modules/spt/getters.js
+++ app/assets/scripts/vuex/modules/spt/getters.js
@@ -1,3 +1,8 @@
 export function products(state,{ _tableDataFilter }) {
     return (store) => _tableDataFilter('store_product_transactions','store',store)
 }
+export function sale(state,getters,rootState,rootGetters) {
+    let direction = 'out', user = rootState['User'].id, date = rootGetters.datetime(), _ref = rootGetters._ref();
+    let nature = rootGetters['TRNS/NameId'].Fresh,type = rootGetters['TRPS/NameId'].Sale;
+    return (store,product,quantity) => { return { direction,nature,type,user,date,store,product,_ref,quantity } }
+}
