9de0512d691d8ea8704bc0ea78b78d8d2c2e8712
Firose Hussain <github@firumon.com>
2019-06-29 22:39:05 +0530

    Created

---
+++ app/components/app/info/AppInfoWideNumerical.vue
@@ -0,0 +1,14 @@
+<template>
+    <GridLayout rows="auto" columns="2*,*">
+        <TextRegular row="0" col="0" class="text-right">{{ title }}</TextRegular>
+        <TextBold row="0" col="1" class="text-right" :text="mText"></TextBold>
+    </GridLayout>
+</template>
+
+<script>
+    export default {
+        name: "AppInfoWideNumerical",
+        props: ['title'],
+        mixins: [require('./../../../assets/scripts/mixins/typography').default],
+    }
+</script>
