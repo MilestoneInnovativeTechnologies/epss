0ca939bf86ac311dee57fbdc7a426147b2427e26
Firose Hussain <github@firumon.com>
2019-06-30 08:18:58 +0530

    price -> amount

--- app/components/pages/sales/SaleDetail.vue
+++ app/components/pages/sales/SaleDetail.vue
@@ -9,9 +9,9 @@
         name: "SaleDetail",
         props: ['id'],
         data(){ return {
-            fields: [ 'docno','date','customer','executive','total','tax','discount','product','quantity','price','pid' ],
-            layoutValues: [ 'product','quantity','price','tax','discount','total' ],
-            cast: { quantity:'quantity',price:'rate',discount:'amount',total:'amount',tax:'rate' },
+            fields: [ 'docno','date','customer','executive','total','tax','discount','product','quantity','amount','pid' ],
+            layoutValues: [ 'product','quantity','amount','tax','discount','total' ],
+            cast: { quantity:'quantity',amount:'rate',discount:'amount',total:'amount',tax:'rate' },
         }},
         computed: {
             layout(){ return _.mapKeys(this.layoutValues,(item) => _.capitalize(item)) },
