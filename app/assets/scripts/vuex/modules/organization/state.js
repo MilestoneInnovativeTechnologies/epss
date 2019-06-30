b3ae82d06ca833439ec60e3f0b70dc8d8b6b66af
Firose Hussain <github@firumon.com>
2019-06-30 12:37:27 +0530

    New print related properties

--- app/assets/scripts/vuex/modules/organization/state.js
+++ app/assets/scripts/vuex/modules/organization/state.js
@@ -1,7 +1,7 @@
 export default {
     table: 'organization', uuid: '',
-    code: null, name: null, image: null,
-    brief: null,
+    code: null, name: null, image: null, brief: null,
+    print_head_text1: null, print_head_text2: null, footer_text: null,
     url_web:null, url_api:null, url_interact:null,
-    fetch: ['code','name','image','brief','url_web','url_api','url_interact']
+    fetch: ['code','name','image','brief','print_head_text1','print_head_text2','footer_text','url_web','url_api','url_interact']
 }
