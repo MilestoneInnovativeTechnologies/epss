5e40397227467d7b52b199a55bc757ecd75ad20d
Firose Hussain <github@firumon.com>
2019-06-30 08:12:06 +0530

    Out, _ref added

--- app/assets/scripts/vuex/modules/spt/getters.js
+++ app/assets/scripts/vuex/modules/spt/getters.js
@@ -2,7 +2,7 @@ export function products(state,{ _tableDataFilter }) {
     return (store) => _tableDataFilter('store_product_transactions','store',store)
 }
 export function sale(state,getters,rootState,rootGetters) {
-    let direction = 'out', user = rootState['User'].id, date = rootGetters.datetime(), _ref = rootGetters._ref();
-    let nature = rootGetters['TRNS/NameId'].Fresh,type = rootGetters['TRPS/NameId'].Sale;
-    return (store,product,quantity) => { return { direction,nature,type,user,date,store,product,_ref,quantity } }
+    let direction = 'Out', user = rootState['User'].id, date = rootGetters.datetime();
+    let nature = rootGetters['TRNS/NameId'].Fresh, type = rootGetters['TRPS/NameId'].Sale;
+    return (store,product,quantity,_ref) => { return { direction,nature,type,user,date,store,product,_ref:(_ref || rootGetters._ref()),quantity } }
 }
