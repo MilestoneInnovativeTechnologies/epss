d7e3a4046036b0ec25c25e23298085ec632ef06f
Firose Hussain <github@firumon.com>
2019-06-30 11:58:49 +0530

    Reserves increment action

---
+++ app/assets/scripts/vuex/modules/reserves/actions.js
@@ -0,0 +1,6 @@
+export function increment({ dispatch,getters,state },id){
+    let data = {}, rData = getters._tableDataItem(state.dbTables[0],id);
+    data['current'] = _.toNumber(rData.current)+1; if(data['current'] > 0 && rData.progress === 'Awaiting') data['progress'] = 'Processing';
+    if((_.toNumber(rData.start_num) + data['current']) >= _.toNumber(rData.end_num)) data['progress'] = 'Completed';
+    dispatch('_update',{ table:'fn_reserves',data,condition:{ id } },{ root:true });
+}
