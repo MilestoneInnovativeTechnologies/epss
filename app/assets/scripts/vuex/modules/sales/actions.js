d60fda9404b8a682a5b8251b7083d05cdf2eac3f
Firose Hussain <github@firumon.com>
2019-06-30 11:59:31 +0530

    Increment added after new sale creation

--- app/assets/scripts/vuex/modules/sales/actions.js
+++ app/assets/scripts/vuex/modules/sales/actions.js
@@ -1,7 +1,8 @@
-export function sale({ dispatch },{ transaction,details,spt }){
+export function sale({ dispatch,commit,rootGetters },{ transaction,details,spt,reserve }){
     return new Promise((resolve,reject) => {
         Promise.all([dispatch('_insert',{ table:'store_product_transactions',data:spt },{ root:true }),dispatch('_insert',{ table:'transactions',data:transaction },{ root:true })]).then(()=>{
             dispatch('_insert',{ table:'transaction_details',data:details },{ root:true });
+            dispatch('Reserves/increment',rootGetters['Reserves/processable'](reserve.fncode,reserve.store)['id'],{ root:true });
             resolve(transaction._ref);
         })
     })
