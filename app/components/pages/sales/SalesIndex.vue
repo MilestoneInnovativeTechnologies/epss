741a50333b0e2fd952cecc7463e85dce7f4c9c3e
Firose Hussain <github@firumon.com>
2019-07-01 06:10:11 +0530

    Sale furnished

--- app/components/pages/sales/SalesIndex.vue
+++ app/components/pages/sales/SalesIndex.vue
@@ -1,12 +1,15 @@
 <template>
     <App title="Sales" action="New Sale" @new-sale="$navigateTo(newSale)">
-        <AppList :source="source" :layout="layout" :links="links" detail="sales/SaleDetail" :title="title"></AppList>
+        <AppMetric :items="metricOne"></AppMetric>
+        <AppMetric :items="metricTwo"></AppMetric>
+        <AppList :source="source" :layout="layout" :links="links" detail="sales/SaleDetail" :title="title" class="m-t-15"></AppList>
     </App>
 </template>
 
 <script>
     import { mapActions,mapState } from 'vuex';
     import {TransactionQueryBuilder} from "../../../assets/scripts/services/transactionquery";
+    import {fetch_total_sale_details_of_a_period} from "../../../assets/scripts/queries";
 
     export default {
         name: "SalesIndex",
@@ -20,15 +23,24 @@
             newSale: require('./SaleNew').default,
         }},
         computed: {
-            ...mapState('Sales',['transactions']),
-            source(){ return __.cast(this[this.root][this.path],this.cast) }
+            ...mapState('Sales',['transactions','metric']), ...mapState('User',['id']),
+            source(){ return __.cast(this[this.root][this.path],this.cast) },
+            startOfDay(){ return moment().startOf('day').format('YYYY-MM-DD HH:mm:ss'); },
+            startOfWeek(){ return moment().startOf('week').format('YYYY-MM-DD HH:mm:ss'); },
+            startOfMonth(){ return moment().startOf('month').format('YYYY-MM-DD HH:mm:ss'); },
+            metricOne(){ return [{ title: 'Total Sales - Today', text:this.getMetricTotalText(this.startOfDay) }] },
+            metricTwo(){ return [{ title: 'Total Sales - Week', text:this.getMetricTotalText(this.startOfWeek) },{ title: 'Total Sales - Month', text:this.getMetricTotalText(this.startOfMonth) }] },
         },
         methods: {
-            ...mapActions('Sales',['_stockIfNot'])
+            ...mapActions('Sales',['_stockIfNot','_stock']),
+            getMetricTotalText(key){ return __.amount(this.metric[key][0]['total'] || '0.00') }
         },
         created() {
             let query = new TransactionQueryBuilder('SL').fields(this.fields).max(this.max).query();
-            this._stockIfNot({ query,key:this.root,path:this.path,on:3 })
+            this._stockIfNot({ query,key:this.root,path:this.path,on:3 });
+            this._stock({ query:sql.format(fetch_total_sale_details_of_a_period,[this.id,this.startOfDay]),key:'metric',path:this.startOfDay });
+            this._stock({ query:sql.format(fetch_total_sale_details_of_a_period,[this.id,this.startOfWeek]),key:'metric',path:this.startOfWeek });
+            this._stock({ query:sql.format(fetch_total_sale_details_of_a_period,[this.id,this.startOfMonth]),key:'metric',path:this.startOfMonth });
         }
     }
 </script>
