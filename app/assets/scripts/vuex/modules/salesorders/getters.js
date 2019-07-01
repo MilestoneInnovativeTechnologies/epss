9fd46a54ef0ff7ab98cc93adfe77d5812e7f72ab
Firose Hussain <github@firumon.com>
2019-07-01 18:34:17 +0530

    Docno getter added

--- app/assets/scripts/vuex/modules/salesorders/getters.js
+++ app/assets/scripts/vuex/modules/salesorders/getters.js
@@ -1,3 +1,9 @@
 export function progressCount({ list }){
     return _.countBy(list,'progress')
 }
+export function docno(state,getters,rootState,rootGetters){
+    return (fncode,fyid) => {
+        let store = rootGetters['User/stores'][0];
+        return rootGetters['Reserves/get'](store,fyid,fncode);
+    }
+}
