a70c6dc4be4ab83ce27aa390d4f73b8cd7614e01
Firose Hussain <github@firumon.com>
2019-07-01 18:34:59 +0530

    Added navigation to create window

--- app/components/pages/sales/SalesOrderIndex.vue
+++ app/components/pages/sales/SalesOrderIndex.vue
@@ -1,22 +1,31 @@
 <template>
-    <App title="Sales Order">
+    <App title="Sales Order" action="New Sales Order" @new-sales-order="$navigateTo(newOrder)">
         <AppList :source="source" :layout="layout" :cast="cast" :links="links" detail="sales/SalesOrderDetail"></AppList>
     </App>
 </template>
 
 <script>
-    import { mapState } from 'vuex';
+    import { mapState,mapActions } from 'vuex';
+    import {user_assigned_customer_sales_orders} from "../../../assets/scripts/queries";
 
     export default {
         name: "SalesOrderIndex",
         data(){ return {
             layout: { Customer:'customer',Date:'date',Status:'progress' },
             cast: { date:'docdate' },
-            links: { customer:['customer/CustomerDetail',{ id:'cid' }] }
+            links: { customer:['customer/CustomerDetail',{ id:'cid' }] },
+            newOrder: require('./OrderNew').default,
         }},
         computed: {
-            ...mapState('SalesOrder',['list']),
+            ...mapState('SalesOrder',['list']),...mapState('User',['id']),
             source(){ return __.cast(this.list,this.cast) }
+        },
+        methods: {
+            ...mapActions('SalesOrder',['_stockIfNot']),
+        },
+        created() {
+            let query = sql.format(user_assigned_customer_sales_orders,this.id);
+            this._stockIfNot({ query,key:'list' })
         }
     }
 </script>
