31e25563fb8bccde4dc324129edfc0c70a5c6e26
Firose Hussain <github@firumon.com>
2019-07-01 11:17:04 +0530

    spelling corrected

--- app/assets/scripts/vuex/modules/organization/state.js
+++ app/assets/scripts/vuex/modules/organization/state.js
@@ -3,5 +3,5 @@ export default {
     code: null, name: null, image: null, brief: null,
     print_head_text1: null, print_head_text2: null, footer_text: null,
     url_web:null, url_api:null, url_interact:null,
-    fetch: ['code','name','image','brief','print_head_line1','print_head_lin32','footer_text','url_web','url_api','url_interact']
+    fetch: ['code','name','image','brief','print_head_line1','print_head_line2','footer_text','url_web','url_api','url_interact']
 }
