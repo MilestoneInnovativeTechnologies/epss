cdf855bad90481025403014b2b86172c8bfa44b5
Firose Hussain <github@firumon.com>
2019-07-01 07:17:54 +0530

    Period mixin utilized

--- app/components/pages/sales/SalesIndex.vue
+++ app/components/pages/sales/SalesIndex.vue
@@ -10,9 +10,11 @@
     import { mapActions,mapState } from 'vuex';
     import {TransactionQueryBuilder} from "../../../assets/scripts/services/transactionquery";
     import {fetch_total_sale_details_of_a_period} from "../../../assets/scripts/queries";
+    import {PeriodDateTime} from "../../../assets/scripts/mixins/perioddatetime";
 
     export default {
         name: "SalesIndex",
+        mixins: [PeriodDateTime],
         data(){ return {
             title: 'Recent Sales',
             fields: ['docno','date','customer','cid','id'],
@@ -25,22 +27,20 @@
         computed: {
             ...mapState('Sales',['transactions','metric']), ...mapState('User',['id']),
             source(){ return __.cast(this[this.root][this.path],this.cast) },
-            startOfDay(){ return moment().startOf('day').format('YYYY-MM-DD HH:mm:ss'); },
-            startOfWeek(){ return moment().startOf('week').format('YYYY-MM-DD HH:mm:ss'); },
-            startOfMonth(){ return moment().startOf('month').format('YYYY-MM-DD HH:mm:ss'); },
             metricOne(){ return [{ title: 'Total Sales - Today', text:this.getMetricTotalText(this.startOfDay) }] },
             metricTwo(){ return [{ title: 'Total Sales - Week', text:this.getMetricTotalText(this.startOfWeek) },{ title: 'Total Sales - Month', text:this.getMetricTotalText(this.startOfMonth) }] },
         },
         methods: {
             ...mapActions('Sales',['_stockIfNot','_stock']),
-            getMetricTotalText(key){ return __.amount(this.metric[key][0]['total'] || '0.00') }
+            getMetricTotalText(key){ return __.amount(_.get(this.metric,[this.path,key,0,'total'],'0,00')) },
+            getMetricPath(key){ return [this.path,key].join('.') },
         },
         created() {
             let query = new TransactionQueryBuilder('SL').fields(this.fields).max(this.max).query();
             this._stockIfNot({ query,key:this.root,path:this.path,on:3 });
-            this._stock({ query:sql.format(fetch_total_sale_details_of_a_period,[this.id,this.startOfDay]),key:'metric',path:this.startOfDay });
-            this._stock({ query:sql.format(fetch_total_sale_details_of_a_period,[this.id,this.startOfWeek]),key:'metric',path:this.startOfWeek });
-            this._stock({ query:sql.format(fetch_total_sale_details_of_a_period,[this.id,this.startOfMonth]),key:'metric',path:this.startOfMonth });
+            this._stock({ query:sql.format(fetch_total_sale_details_of_a_period,[this.id,this.startOfDay]),key:'metric',path:this.getMetricPath(this.startOfDay) });
+            this._stock({ query:sql.format(fetch_total_sale_details_of_a_period,[this.id,this.startOfWeek]),key:'metric',path:this.getMetricPath(this.startOfWeek) });
+            this._stock({ query:sql.format(fetch_total_sale_details_of_a_period,[this.id,this.startOfMonth]),key:'metric',path:this.getMetricPath(this.startOfMonth) });
         }
     }
 </script>
