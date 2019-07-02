a162b2231db4b7976bf222a2008d596b1f182b8e
Firose Hussain <github@firumon.com>
2019-07-02 10:54:30 +0530

    Return sale action

--- app/assets/scripts/vuex/modules/sales/actions.js
+++ app/assets/scripts/vuex/modules/sales/actions.js
@@ -7,3 +7,12 @@ export function sale({ dispatch,commit,rootGetters },{ transaction,details,spt,r
         })
     })
 }
+export function sReturn({ dispatch,commit,rootGetters },{ transactions,transaction_details,store_product_transactions }){
+    return new Promise((resolve,reject) => {
+        Promise.all([dispatch('_insert',{ table:'store_product_transactions',data:store_product_transactions },{ root:true }),dispatch('_insert',{ table:'transactions',data:transactions },{ root:true })]).then(()=>{
+            dispatch('_insert',{ table:'transaction_details',data:transaction_details },{ root:true });
+            dispatch('Reserves/increment',rootGetters['Reserves/processable'](transactions.fncode,store_product_transactions[0].store)['id'],{ root:true });
+            resolve(transactions._ref);
+        })
+    })
+}
