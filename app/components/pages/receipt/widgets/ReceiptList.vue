fd1df9d5b89959e1a9438f312d3916a3cfb475fc
Firose Hussain <github@firumon.com>
2019-07-03 19:45:57 +0530

    limit and title added

--- app/components/pages/receipt/widgets/ReceiptList.vue
+++ app/components/pages/receipt/widgets/ReceiptList.vue
@@ -1,11 +1,14 @@
 <template>
-    <AppList :source="receipts" :layout="listLayout" :cast="listCast" :detail="listDetail" :links="listLinks"></AppList>
+    <AppList :source="receipts" :layout="listLayout" :cast="listCast" :detail="listDetail" :links="listLinks"  :title="title" :limit="limit"></AppList>
 </template>
 
 <script>
+    import { mapActions } from 'vuex';
+    import {fetch_all_active_receipts} from "../../../../assets/scripts/queries";
+
     export default {
         name: "ReceiptList",
-        props: ['receipts','layout','cast','detail','links'],
+        props: ['receipts','layout','cast','detail','links','title','limit'],
         data(){ return {
             defaultLayout: { Customer:'customer',Date:'date',Amount:'amount' },
             defaultCast: { date:'docdate',amount:'amount' },
@@ -17,6 +20,10 @@
             listCast(){ return this.cast || this.defaultCast },
             listDetail(){ return this.detail || this.defaultDetail },
             listLinks(){ return this.links || this.defaultLinks },
+        },
+        methods: mapActions('Receipts',['_stockIfNot']),
+        created() {
+            this._stockIfNot({ query:sql.format(fetch_all_active_receipts) })
         }
     }
 </script>
