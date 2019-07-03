8e07df9a95787788938df9690d0931857e155317
Firose Hussain <github@firumon.com>
2019-07-03 18:57:34 +0530

    Receipt List common widget created

---
+++ app/components/pages/receipt/widgets/ReceiptList.vue
@@ -0,0 +1,22 @@
+<template>
+    <AppList :source="receipts" :layout="listLayout" :cast="listCast" :detail="listDetail" :links="listLinks"></AppList>
+</template>
+
+<script>
+    export default {
+        name: "ReceiptList",
+        props: ['receipts','layout','cast','detail','links'],
+        data(){ return {
+            defaultLayout: { Customer:'customer',Date:'date',Amount:'amount' },
+            defaultCast: { date:'docdate',amount:'amount' },
+            defaultDetail: 'receipt/ReceiptDetail',
+            defaultLinks: { customer:['customer/CustomerDetail',{ id:'cid' }] },
+        }},
+        computed: {
+            listLayout(){ return this.layout || this.defaultLayout },
+            listCast(){ return this.cast || this.defaultCast },
+            listDetail(){ return this.detail || this.defaultDetail },
+            listLinks(){ return this.links || this.defaultLinks },
+        }
+    }
+</script>
