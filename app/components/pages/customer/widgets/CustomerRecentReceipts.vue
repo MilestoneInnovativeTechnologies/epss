22e86a513984e7379fbfdda21496e70d56fe2651
Firose Hussain <github@firumon.com>
2019-07-03 19:46:26 +0530

    Widget for customer receipts

---
+++ app/components/pages/customer/widgets/CustomerRecentReceipts.vue
@@ -0,0 +1,19 @@
+<template>
+    <ReceiptList :receipts="receipts" :layout="layout" :title="title" :limit="limit"></ReceiptList>
+</template>
+
+<script>
+    import { mapState } from 'vuex';
+
+    export default {
+        name: "CustomerRecentReceipts",
+        props: ['id','title','limit'],
+        data(){ return {
+            layout: { Date:'date',Type:'mode',Amount:'amount' }
+        }},
+        computed: {
+            ...mapState('Receipts',{ receiptList:'list' }),
+            receipts(){ return this.receiptList.filter(receipt => receipt.cid == this.id) }
+        }
+    }
+</script>
