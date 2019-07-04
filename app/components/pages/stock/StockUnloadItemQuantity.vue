40b9b363d899ef93abafb37d44ea35f029847bc4
Firose Hussain <github@firumon.com>
2019-07-04 09:19:13 +0530

    Changing Out Quantity - Not completed

---
+++ app/components/pages/stock/StockUnloadItemQuantity.vue
@@ -0,0 +1,21 @@
+<template>
+    <App title="Change Stock unload quantity">
+<!--        <AppForm action="Done"></AppForm>-->
+    </App>
+</template>
+
+<script>
+    import AppForm from "../../app/form/AppForm";
+    const feMX = require('./../../../assets/scripts/mixins/formelement');
+
+    export default {
+        name: "StockUnloadItemQuantity",
+        // mixins: [feMX.common, feMX.quantity],
+        props: ['id','max','current','delegate'],
+        methods: {
+            fields(){
+                //let fields =
+            }
+        }
+    }
+</script>
