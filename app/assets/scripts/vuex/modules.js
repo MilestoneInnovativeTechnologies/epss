e97182dd44bd0fd8d2539efccd7d7e4bd4032dc0
Firose Hussain <github@firumon.com>
2019-07-07 13:09:35 +0530

    Added app module and removed Org,Database modules

--- app/assets/scripts/vuex/modules.js
+++ app/assets/scripts/vuex/modules.js
@@ -1,8 +1,9 @@
 export default {
+    'App': require('./modules/app/index').default,
     'Log': require('./modules/log/index').default,
     'Axios': require('./modules/axios/index').default,
-    'Organization': require('./modules/organization/index').default,
-    'Database': require('./modules/database/index').default,
+    // 'Organization': require('./modules/organization/index').default,
+    // 'Database': require('./modules/database/index').default,
     'Sync': require('./modules/sync/index').default,
     'User': require('./modules/user/index').default,
     'Menu': require('./modules/menu/index').default,
