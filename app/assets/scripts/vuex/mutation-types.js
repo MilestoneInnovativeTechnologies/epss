1b761cd0ec176b5d338b0774566c3591dd59de24
Firose Hussain <github@firumon.com>
2019-07-07 13:09:06 +0530

    Deleted unused mutations

--- app/assets/scripts/vuex/mutation-types.js
+++ app/assets/scripts/vuex/mutation-types.js
@@ -2,48 +2,38 @@
 export const add_module = 'addModule';
 export const bind_table_module = 'bindTableModule';
 
+//App
+
 //Helper
 
 //Module Common
 export const mutate_sync_data = 'mutateSyncData';
 export const stock_state_data = 'stockStateData';
 export const increment_stock_cache = 'incrementStockCache';
+export const set_state_data = 'setStateData';
 
 export const add_message_to_log_queue = 'addQueue';
 
-export const set_device_uuid = 'set_uuid';
-export const set_organization_details = 'setInfo';
 export const add_configuration_to_server_queue = 'addQueue';
 export const start_process_queue = 'initQueueProcess';
 export const initiate_processing_transfer = 'initTransfer';
 export const finalize_processing_transfer = 'finalizeTransfer';
 export const finalize_failed_transfer = 'failedTransfer';
-export const populate_organisation_data = 'setOrganisation';
-export const populate_axios_url_data = 'setAxiosUrlData';
 
 //Axios
 export const set_connectivity_availability = 'setConnectivityAvailable';
-export const update_axios_user_details  = 'setUserDetails';
 
 
 //Sync
 export const add_new_table_for_sync = 'newTable';
 export const add_to_app_sync_queue = 'addToAppQueue';
 export const processing_queue = 'processQueue';
-export const set_sync_base_url = 'setBaseUrl';
 export const finish_processing_queue = 'finishProcessingSync';
 export const set_repeat_failed_timeout = 'setRepeatFailedTimeout';
 export const update_table_timing = 'updateTableTime';
 export const set_new_sync_time_out = 'setTimeOut';
-export const update_sync_user_details = 'setUserDetails';
 
 //User
-export const set_user_email = 'setEmail';
-export const set_user_password = 'setPassword';
 export const set_user_pin = 'setPin';
-export const update_login_details = 'updateLoginDetails';
-export const init_login_validation = 'initLogin';
-export const login_validation_failed = 'loginFailed';
-export const clear_login_message = 'clearLoginMessage';
 
 export const MUTATE = 'DEMO';
