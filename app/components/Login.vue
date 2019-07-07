d222047afad7b71210142e3ca67d298adee0a78a
Firose Hussain <github@firumon.com>
2019-07-07 13:12:56 +0530

    Login component for handling login sessions

--- app/components/Login.vue
+++ app/components/Login.vue
@@ -1,47 +1,80 @@
 <!--suppress ALL -->
 <template>
-    <StackLayout width="75%" class="m-t-20">
-        <AbsoluteLayout>
-            <StackLayout width="100%">
-                <TextHeading class="text-center">LOGIN</TextHeading>
-                <TextField v-model="email" hint="Email" class="cp fsi" @returnPress="focusPassword" />
-                <TextField v-model="password" hint="Password"  @returnPress="login" class="cp fsi" secure="true" ref="password" />
-                <Button text="Login" width="50%" :isEnabled="!validating" @tap="login" class="btn btn-primary btn-active bcp" />
-                <Button text="Home" width="50%" @tap="$navigateTo(Home)" class="btn btn-primary btn-active bcp" />
-                <Button text="DEBUG" width="50%" @tap="$navigateTo(Debug)" class="btn btn-primary btn-active bcp" />
+    <App title="Login" :action="loginForm ? 'Login' : null" @login="authenticate" width="100%">
+        <GridLayout rows="auto,auto,*" class="w-full">
+            <Organization row="0"></Organization>
+            <LoginForm row="1" width="85%" class="m-t-30" v-if="loginForm"></LoginForm>
+            <StackLayout row="2" class="m-t-8 w-full">
+                <ActivityIndicator :busy="busy" class="m-t-10"></ActivityIndicator>
+                <TextHighlight class="w-full text-center" v-for="(pTxt,idx) in pTexts" :text="pTxt" :key="'ull-'+idx"></TextHighlight>
             </StackLayout>
-            <StackLayout width="100%" height="175">
-                <ActivityIndicator :busy="validating" class="m-t-30"></ActivityIndicator>
-            </StackLayout>
-        </AbsoluteLayout>
-    </StackLayout>
+        </GridLayout>
+    </App>
 </template>
 
 <script>
-    import {set_user_email, set_user_password, clear_login_message } from './../assets/scripts/vuex/mutation-types'
-    import { createNamespacedHelpers } from 'vuex';
-    const { mapGetters,mapActions,mapState,mapMutations } = createNamespacedHelpers('User');
-    import Home from './Home'; import Debug from './Debug';
+    import { mapGetters,mapActions,mapState,mapMutations } from 'vuex';
+    import {set_state_data} from "../assets/scripts/vuex/mutation-types";
+
     export default {
         name: 'Login',
-        data: () => _.zipObject(['Home','Debug'],[Home,Debug]),
-        computed:{
-            ...mapState(['validating','message']),
-            ...mapGetters(['credentials']),
-            email:{ get(){ return this.credentials.email },  set(val){ this.$store.commit('User/' + set_user_email,val) } },
-            password:{ get(){ return this.credentials.password },  set(val){ this.$store.commit('User/' + set_user_password,val) } },
+        data(){ return {
+            busy: false,
+            loginForm: false,
+            pTexts: [],
+            maxHomeNavDelay: 15,
+        }},
+        computed: {
+            ...mapState('User',['message','validating','id']),...mapState('Sync',['queue_index']),
+            ...mapState('Sync',{ SyncTables: 'tables',SyncQueue:'queue',SyncQIndex:'queue_index',SyncProcessing:'processing',SyncURL:'url',SyncTime:'time',SyncClient:'client',SyncUser:'user' }),
+            ...mapState('Axios',{ AxiosConnection: 'connection', AxiosQueue: 'queue', AxiosProcessing: 'processing', AxiosStatus: 'status', AxiosTransfer: 'transfer', AxiosConfig: 'config', AxiosURLInteract: 'url_interact', AxiosURLApi: 'url_api', AxiosRRLSync: 'url_sync', AxiosSuccess: 'success', AxiosFail: 'fail', AxiosSyncConfig: 'sync_config', AxiosApiConfig: 'api_config', AxiosLastResponse: 'last_response',AxiosQueue_failed: 'queue_failed',AxiosClient: 'client' }),
+            authenticated(){ let id = this.id; return !(_.isNil(id)) }
         },
         methods: {
-            ...mapActions(['login']),
-            focusPassword(){ this.$refs.password.nativeView.focus() },
-            ...mapMutations([clear_login_message]),
+            ...mapActions('User',['doLogin','doLoginActions']), ...mapMutations('User',[set_state_data]),
+            pTxt(txt){ this.pTexts.push(txt) },
+            authenticate(){
+                this.pTxt('Authenticating with server....');
+                this.doLogin();
+            },
+            checkLogin(){
+                this.pTxt('Checking loging details in Database..'); this.busy = true;
+                DB.get('user',null,function(vm){
+                    if(this.error) return setTimeout((vm) => vm.$navigateTo(require('./Setup').default,{ backstackVisible:false }),500,vm);
+                    if(_.isEmpty(this.result)) return vm.initLogin();
+                    vm.populateUserData(this.result)
+                },this)
+            },
+            redirectToHome(after){
+                this.pTxt(`You will be redirected to home page within ${after} seconds!!`);
+                setTimeout(() => this.$navigateTo(require('./Home').default),after*1000);
+            },
+            populateUserData(data){
+                this.pTxt('User data found, populating..');
+                let kData =_(data).keyBy('name').mapValues(({ detail }) => detail).value();
+                this.pTxt('Doing post login actions..');
+                this.doLoginActions(kData).then(() => this.redirectToHome(this.delay()));
+            },
+            initLogin(){
+                this.pTxt('No data found, try loging in using form..');
+                this.loginForm = true; this.busy = false;
+            },
+            postFormLogin(data){
+                this.busy = true; this.loginForm = false;
+                this.pTxt(`Login Success!! User data Synching in progress`);
+                setTimeout(() => this.redirectToHome(this.delay()),3000);
+            },
+            delay(){
+                let qi = this.queue_index;
+                return _(qi).map(i => parseInt(i) - __.now()).filter(i => i<this.maxHomeNavDelay).max() || 1;
+            },
         },
         mounted(){
-            this.email = 'SE01.sls@temp.mail';
-            this.password = '123456';
+            this.$nextTick(() => this.checkLogin())
         },
         watch: {
-            message:function(val){ if(_.isEmpty(val)) return; alert({ title:'Login Error', message:val, okButtonText:'Ok' }).then(()=>this[clear_login_message]()) }
-        },
+            message(message){ if(_.isEmpty(message)) return; alert({ title:'Login Error',message,okButtonText:"Ok" }).then(() => this[set_state_data]({ message:'' })) },
+            authenticated(status){ if(status === true) this.postFormLogin() },
+        }
     }
 </script>
