11124933267232f2b84ea4ce06eaa9b70bb70267
Firose Hussain <github@firumon.com>
2019-07-08 08:07:46 +0530

    Devtools disabled for debug build

--- app/main.js
+++ app/main.js
@@ -8,10 +8,10 @@ global.FD = require('./assets/scripts/services/formdata').FD;
 import "./app.scss";
 import "./app.css";
 
-Vue.config.devtools = true;
-import VueDevtools from 'nativescript-vue-devtools'
-if(TNS_ENV !== 'production') { Vue.use(VueDevtools, { host:'192.168.43.48' }); }
-Vue.config.silent = (TNS_ENV === 'production'); // Prints Vue logs when --env.production is *NOT* set while building
+// Vue.config.devtools = true;
+// import VueDevtools from 'nativescript-vue-devtools'
+// if(TNS_ENV !== 'production') { Vue.use(VueDevtools, { host:'192.168.43.48' }); }
+// Vue.config.silent = (TNS_ENV === 'production'); // Prints Vue logs when --env.production is *NOT* set while building
 
 import store from './assets/scripts/vuex/store';
 global.log = function(text,...texts){ store.commit('Log/addQueue',text); if(TNS_ENV !== 'production') console.log(text,...texts); };
