f81346db51f026f46f2fe8da75e12c2a65b6f9dd
Firose Hussain <github@firumon.com>
2019-07-08 07:26:40 +0530

    Defaults implemented

--- app/components/button/AppButton.vue
+++ app/components/button/AppButton.vue
@@ -1,10 +1,17 @@
 <template>
-    <Button class="bcp fsb" height="60" :text="mText"></Button>
+    <Button class="fsb" :class="btnCls" :height="height" :text="mText"></Button>
 </template>
 
 <script>
     export default {
         name: "AppButton",
         mixins: [require('./../../assets/scripts/mixins/typography').default],
+        props: {
+            height: { default:60 },
+            bcc: { default:'bcp' }
+        },
+        computed: {
+            btnCls(){ return [this.bcc]; },
+        }
     }
 </script>
