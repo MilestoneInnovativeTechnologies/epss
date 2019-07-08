a515ba8e3f8eb2e16fa15cc3c671fb4c37b4c538
Firose Hussain <github@firumon.com>
2019-07-08 07:31:54 +0530

    RadSideDrawer required into project

--- app/components/index.js
+++ app/components/index.js
@@ -1,13 +1,9 @@
 import Vue from 'nativescript-vue';
 
 import RadDataForm from 'nativescript-ui-dataform/vue';
-
-
-
-
-
-
 Vue.use(RadDataForm);
+
+Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer);
 
 const files = require.context('./', true, /\.vue$/i);
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
