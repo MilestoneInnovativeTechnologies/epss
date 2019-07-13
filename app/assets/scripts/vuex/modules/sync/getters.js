f90b46f6db4ba1e74188893e876e2f3060d8839e
Firose Hussain <github@firumon.com>
2019-07-13 08:00:37 +0530

    Syncing happens only when client available

--- app/assets/scripts/vuex/modules/sync/getters.js
+++ app/assets/scripts/vuex/modules/sync/getters.js
@@ -1,6 +1,6 @@
 export function haveQueue({ queue_index }){ return queue_index.length !== 0 }
 export function haveQueueEarlierThan({ queue_index }){ return (time) => time >= _.head(queue_index); }
-export function canStartProcessingQueue({ processing }){ return _.isEmpty(processing); }
+export function canStartProcessingQueue({ processing },getters,rootState){ return (_.isEmpty(processing) && !!rootState['App'].uuid); }
 export function getFirstQueueItem({ queue_index,queue }){
     let index = _.head(queue_index), item = queue[index];
     return { item,index };
