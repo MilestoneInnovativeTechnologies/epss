b006d5860be80af4dc78bd2698be78079a79e5f2
Firose Hussain <github@firumon.com>
2019-07-13 12:53:42 +0530

    Rearranged..

--- app/main.js
+++ app/main.js
@@ -3,8 +3,7 @@ import Home from './components/Home'
 import {TNSFontIcon, fonticon} from 'nativescript-fonticon';
 
 global._ = require('lodash'); global.moment = require('moment'); global.__ = {}; global.sql = require('sqlstring');
-global.DB = require('./assets/scripts/services/database').DB;
-global.FD = require('./assets/scripts/services/formdata').FD;
+global.DB = require('./assets/scripts/services/database').DB; global.FD = require('./assets/scripts/services/formdata').FD;
 
 import "./app.scss";
 import "./app.css";
@@ -13,11 +12,11 @@ import store from './assets/scripts/vuex/store';
 global.log = function(text,...texts){ store.commit('Log/addQueue',text); if(TNS_ENV !== 'production') console.log(text,...texts); };
 
 // import VueDevtools from 'nativescript-vue-devtools'
-
 // if(TNS_ENV !== 'production') {
 //   Vue.use(VueDevtools,{ host:'192.168.43.48' })
 // }
 // Prints Vue logs when --env.production is *NOT* set while building
+
 Vue.config.silent = (TNS_ENV === 'production');
 
 // TNSFontIcon.debug = true;
