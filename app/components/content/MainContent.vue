cac5ac2c41fc2200625b12bcf93d7a7243a2dd06
Firose Hussain <github@firumon.com>
2019-07-13 08:02:42 +0530

    Component to accommodate main contents of a page

---
+++ app/components/content/MainContent.vue
@@ -0,0 +1,32 @@
+<template>
+    <GridLayout :rows="rows">
+        <ScrollView v-if="scroll !== 'false' && scroll !== false" row="0"><StackLayout :verticalAlignment="vAlign" class="p-t-10" :width="width"><slot></slot></StackLayout></ScrollView>
+        <StackLayout v-else :verticalAlignment="vAlign" row="0" class="p-t-10" :width="width"><slot></slot></StackLayout>
+        <StackLayout row="1" orientation="horizontal"><AppButton class="c-white btn-active" v-bind="bindVars(btn)" :width="actWidth" v-for="(btn,idx) in actions" @tap.native="$emit(eventName(btn))" :key="['app',unique,'btn',idx].join('-')">{{ btn }}</AppButton></StackLayout>
+    </GridLayout>
+</template>
+
+<script>
+    export default {
+        name: "MainContent",
+        props: ['action','scroll','width','center','actionProps'],
+        data(){ return {
+            fixHeight: 60,
+        }},
+        computed: {
+            unique(){ return new Date().getTime() },
+            rows(){ let rows = ['*'], action = !!this.action; rows.push(_.toSafeInteger(action)*this.fixHeight); return rows.join(','); },
+            vAlign(){ return (this.center === true || this.center === 'true') ? 'center' : 'top' },
+            actions(){ let actions = this.action; return (actions) ? _.concat(actions) : []; },
+            actWidth(){ return _.toSafeInteger(100/this.actions.length)+'%' }
+        },
+        methods: {
+            eventName(name){ return _.kebabCase(name) },
+            bindVars(act){
+                let name = this.eventName(act); return (_.isEmpty(this.actionProps))
+                    ? {}
+                    : ( _.has(this.actionProps,name) ? this.actionProps[name] : this.actionProps )
+            }
+        }
+    }
+</script>
