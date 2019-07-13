10c754c4ae80b028826a25b6b0bf62edca053bc7
Firose Hussain <github@firumon.com>
2019-07-13 08:01:57 +0530

    Navigation Spinner when any navigation happens

---
+++ app/components/content/NavigationSpinner.vue
@@ -0,0 +1,11 @@
+<template>
+    <GridLayout rows="*" columns="*" style="background-color: rgba(255,255,255,0.6)">
+        <ActivityIndicator :busy="true" verticalAlignment="center" horizontalAlignment="center"></ActivityIndicator>
+    </GridLayout>
+</template>
+
+<script>
+    export default {
+        name: "NavigationSpinner",
+    }
+</script>
