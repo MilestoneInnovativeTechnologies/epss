3ad1993b2ac184bdcaad94757d1a806fae5115ce
Firose Hussain <github@firumon.com>
2019-07-22 20:07:02 +0530

    Result fetching and proper call back

--- app/assets/scripts/vuex/actions.js
+++ app/assets/scripts/vuex/actions.js
@@ -19,10 +19,11 @@ export function redrawModules({state, commit, rootState}, table) {
 export function _insert({ dispatch,commit,state },{ table,data,success,vm }){
     return new Promise((res) => {
         DB.insert(table,data,function (table,callback,vm,mutation,commit,dispatch,res) {
+            let result = this.result;
             commit('Sync/' + mutation,{ table,type:'create' },{ root:true });
             dispatch('redrawModules',table,{ root:true });
-            if(callback) callback.call(vm,this.result);
-            res(this.result);
+            if(callback) if(_.isFunction(callback)) callback.call(vm,result); else vm[callback].call(vm,result);
+            res(result);
         },table,success,vm,update_table_timing,commit,dispatch,res)
     });
 }
@@ -31,9 +32,10 @@ export function _update({ dispatch,commit },{ table,data,id,pk,condition }){
     condition = condition || (_.zipObject([(pk || 'id')],[id]));
     return new Promise((res, rej) => {
         DB.update(table, condition, data, function (table, mutation, commit, dispatch, res) {
+            let result = this.result;
             commit('Sync/' + mutation, {table, type: 'update'}, {root: true});
             dispatch('redrawModules', table, {root: true});
-            res(this.result);
+            res(result);
         }, table, update_table_timing, commit, dispatch,res);
     });
 }
