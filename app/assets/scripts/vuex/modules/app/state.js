a8cb8e9b499a2311f8eaa1a47bb68cb413a40c49
Firose Hussain <github@firumon.com>
2019-07-23 07:25:39 +0530

    New task, syncing tables added

--- app/assets/scripts/vuex/modules/app/state.js
+++ app/assets/scripts/vuex/modules/app/state.js
@@ -2,7 +2,7 @@ export default {
     dbTables:['app','user'], fields:'name,detail',
     tasks: { 'Init state data':false, 'Request device registration data':false, 'Create app table':false,
         'Insert device registration data':false,'Initialize app':false, 'Create user table':false,
-        'Reset client on server':false, 'Get DB tables':false,'Create and sync tables':false },
+        'Reset client on server':false, 'Get DB tables':false,'Create tables':false,'Synchronize tables':false },
     message: '', uuid: '', height: '', width:'',
     code: '', name: null, image: null, brief: null,
     print_head_line1: null, print_head_line2: null, footer_text: null,
