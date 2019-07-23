e2c7bda7a3d8ceb08722cbc0931205e0644f9239
Firose Hussain <github@firumon.com>
2019-07-23 09:09:54 +0530

    Debug change removed

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -51,7 +51,6 @@
                 if(vm.id){ this.start() }
                 else { setTimeout(() => vm.login(),2000); }
             })
-        },
-        created(){ DB.drop('user');DB.drop('app'); }
+        }
     }
 </script>
