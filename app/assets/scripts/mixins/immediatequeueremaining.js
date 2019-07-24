ac3a9657f7e92334ba781583f9e7642d5a2930c2
Firose Hussain <github@firumon.com>
2019-07-24 18:25:40 +0530

    Queue Index Wait, corrected

--- app/assets/scripts/mixins/immediatequeueremaining.js
+++ app/assets/scripts/mixins/immediatequeueremaining.js
@@ -1,27 +1,31 @@
-import {gap_between_sync_queue_seconds} from "../constants";
+import { gap_between_sync_queue_seconds,approx_time_for_a_sync } from './../constants';
 import { mapState } from 'vuex';
 
 export const ImmediateQueueRemainingTimeMixin = {
     data(){ return {
         queueRemainingTime: 0,
         immediateQueueFinished: false,
+        queueBatchLastItem: null,
     }},
     computed: {
         ...mapState('Sync',{syncQueueTimeIndex:'queue_index'}),
     },
     methods: {
-        getQueueRemainingTime(queue){ return _.reduce(queue,(prev,next) => ((tInt(next) - tInt(prev)) > gap_between_sync_queue_seconds) ? tInt(prev) : tInt(next),queue[0]) - tInt(new Date().getTime() / 1000) },
+        getQueueRemainingTime(queue){ return this.getBatchLastQueueItem(queue) - now() },
+        getBatchLastQueueItem(Q){ let P = now(); for(let x in Q) if(tInt(Q[x] - P) > gap_between_sync_queue_seconds) return P; else P = Q[x]; return tInt(_.last(Q)) },
     },
     watch: {
         syncQueueTimeIndex(queue){
-            if(tInt(queue[0]) - tInt(new Date().getTime() / 1000) > gap_between_sync_queue_seconds){
+            let fstTime = tInt(queue[0]), lstTime = this.queueBatchLastItem || (this.queueBatchLastItem = this.getBatchLastQueueItem(queue)), tmeRemaining = lstTime - now();
+            if(fstTime <= lstTime){
+                this.immediateQueueFinished = false;
+                this.queueRemainingTime = (tmeRemaining < 0) ? ('~' + (approx_time_for_a_sync * (queue.indexOf(lstTime)+1))) : tmeRemaining;
+            } else {
                 this.immediateQueueFinished = true;
                 this.queueRemainingTime = 0;
-            } else {
-                this.queueRemainingTime = this.getQueueRemainingTime(queue);
-                this.immediateQueueFinished = false;
             }
         }
     }
 };
 function tInt(n){ return _.toSafeInteger(n) }
+function now(){ return parseInt(new Date().getTime()/1000) }
