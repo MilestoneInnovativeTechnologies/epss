9881f9742f669b0e97d320013a2ac0417b6ab582
Firose <firumon@github.com>
2019-08-10 18:31:53 +0530

    Stock load code optimized for immediate sync and proper insert like one by one

--- app/assets/scripts/vuex/modules/stock/actions.js
+++ app/assets/scripts/vuex/modules/stock/actions.js
@@ -10,8 +10,12 @@ export function transferOut({ dispatch },data){
 export function transferIn({ dispatch },data){
     return new Promise((resolve, reject) => {
         dispatch('entry',data).then(({ store,fncode,transactions }) => {
-            dispatch('Reserves/incReserve',{fncode, store},{ root:true });
-            dispatch('_update',{ table:'stock_transfer',data:{ in:transactions._ref },condition:{ out:data.id } },{ root:true });
+            Promise.all([
+                dispatch('Reserves/incReserve',{fncode, store},{ root:true }),
+                dispatch('_update',{ table:'stock_transfer',data:{ in:transactions._ref },condition:{ out:data.id } },{ root:true }),
+            ]).then(() => {
+                dispatch('Sync/priorSyncQueue',['transactions','store_product_transactions','transaction_details','stock_transfer','fn_reserves'],{ root:true });
+            });
             resolve(transactions);
         })
     });
@@ -22,8 +26,9 @@ export function entry({ dispatch },{ transactions,transaction_details,store_prod
             dispatch('_insert',{ table:'transactions',data:transactions },{ root:true }),
             dispatch('_insert',{ table:'store_product_transactions',data:store_product_transactions },{ root:true }),
         ]).then(() => {
-            dispatch('_insert',{ table:'transaction_details',data:transaction_details },{ root:true });
-            resolve({ transactions,store:store_product_transactions[0]['store'],fncode:transactions.fncode })
+            dispatch('_insert',{ table:'transaction_details',data:transaction_details },{ root:true }).then(() => {
+                resolve({ transactions,store:store_product_transactions[0]['store'],fncode:transactions.fncode })
+            });
         })
     })
 }
