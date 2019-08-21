ee6a8832c89da7098b4acd342a69e784f5561169
Firose <firumon@github.com>
2019-08-21 16:46:39 +0530

    Removed unused state keys

--- app/assets/scripts/vuex/modules/sync/state.js
+++ app/assets/scripts/vuex/modules/sync/state.js
@@ -2,14 +2,7 @@ export default {
     tables: {},
     queue: {},
     queue_index: [],
-    // app_sync_start_after: 5,
-    // repeat_failed_processing_seconds: 60,
     processing: {},
-    // processing: 'pause', // Pause sync while development
     url: '',
     time: {},
-    // repeat_failed_timeout: 0,
-    // time_out: 0,
-    // user: '',
-    // client: ''
 }
