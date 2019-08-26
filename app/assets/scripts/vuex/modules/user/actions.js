c7e5afd9d4d1cbb15e64a69a7171604439548354
Firose <firumon@github.com>
2019-08-26 21:53:58 +0530

    Login modifications

--- app/assets/scripts/vuex/modules/user/actions.js
+++ app/assets/scripts/vuex/modules/user/actions.js
@@ -4,25 +4,38 @@ const userStateKeys = ['name','email','id','api_token','reference'];
 
 export function doLogin({ state,commit,dispatch }){
     let { email,password } = state; commit(set_state_data,{ validating:true });
-    dispatch('api',{ item:'login', params: { email,password }, success: 'User/doLoginActions' },{ root:true })
+    dispatch('api',{ item:'login', params: { email,password }, success: 'User/loginResponse' },{ root:true })
 }
 
-export function doLoginActions({ commit,dispatch },data) {
-    return new Promise((resolve, reject) => {
-        if(_.isEmpty(data)) return reject(commit(set_state_data,{ validating:false,message:'No email and password matching records found' }));
-        let insData = _.map(data,(detail,name) => _.zipObject(['name','detail'],[name,detail]));
-        dispatch('_insert',{ table:'user',data:insData },{ root:true }).then(() => {
-            commit(set_state_data,_.pick(data,userStateKeys));
-            dispatch('Sync/initUserTables',null,{ root:true }).then((data) => resolve(data));
-            commit(set_state_data,{ validating:false });
+export function loginResponse({ commit,dispatch }, data) {
+    if(_.isEmpty(data)) return commit(set_state_data,{ validating:false,message:'No email and password matching records found' });
+    dispatch('doInsertLoginData',getNameDetailArray(data))
+}
+export function doInsertLoginData({ dispatch },data){
+    return new Promise((resolve) => {
+        dispatch('_insert',{ table:'epss_user',data:data },{ root:true }).then((activity) => {
+            dispatch('doPostLoginActions',getNameDetailObject(activity.data));
         });
     })
 }
+export function doPostLoginActions({ commit,dispatch },data) {
+    commit(set_state_data,_.pick(data,userStateKeys));
+    dispatch('Sync/initUserTables',null,{ root:true });
+    commit(set_state_data,{ validating:false });
+}
 export function logout({ commit },vm) {
     return new Promise((resolve, reject) => {
-        DB.delete('user',null,function(resolve,vm,commit){
+        DB.delete('epss_user',null,function(resolve,vm,commit){
             commit(set_state_data,_(userStateKeys).mapKeys(key => key).mapValues(() => null).value());
             resolve(vm);
         },resolve,vm,commit)
     });
+}
+
+function getNameDetailArray(obj){
+    return _.map(obj,(detail,name) => _.zipObject(['name','detail'],[name,detail]))
+}
+
+function getNameDetailObject(ary){
+    return _(ary).mapKeys('name').mapValues('detail').value();
 }
