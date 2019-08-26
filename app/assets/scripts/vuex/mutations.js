09a427c6fd412e951523fba22f7a2d5a13ff2ca5
Firose <firumon@github.com>
2019-08-26 21:53:15 +0530

    Removed unused mutations

--- app/assets/scripts/vuex/mutations.js
+++ app/assets/scripts/vuex/mutations.js
@@ -1,10 +1,9 @@
 import {
-    add_module, bind_table_module, add_connection_monitor, create_event_subscription, add_event_subscriber, remove_event_subscriber
+    add_module, bind_table_module, create_event_subscription, add_event_subscriber, remove_event_subscriber
 } from './mutation-types';
 
 export default {
     [add_module](state, mod) { state.modules.push(mod); },
-    [add_connection_monitor](state, action) { state.connection_monitors.push(action); },
     [bind_table_module](state, { module,table }) {
         if(!_.has(state.module_tables,module)) Object.assign(state.module_tables,_.zipObject([module],[[]]));
         let tables = _.isArray(table) ? table : [table]; Array.prototype.push.apply(state.module_tables[module],tables);
