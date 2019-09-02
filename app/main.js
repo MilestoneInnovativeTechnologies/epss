dddd0dd3c089d3d87cbcf2c9d5fe8a938c475e03
Firose <firumon@github.com>
2019-09-02 17:47:30 +0530

    Debug, VueDevtool IP corrected

--- app/main.js
+++ app/main.js
@@ -13,8 +13,9 @@ global.log = function(text,...texts){ store.commit('Log/addQueue',text); if(TNS_
 
 import VueDevtools from 'nativescript-vue-devtools'
 if(TNS_ENV !== 'production') {
-  Vue.use(VueDevtools,{ host:'192.168.10.7' })
+  Vue.use(VueDevtools,{ host:'192.168.10.11' })
 }
+
 // Prints Vue logs when --env.production is *NOT* set while building
 
 Vue.config.silent = (TNS_ENV === 'production');
