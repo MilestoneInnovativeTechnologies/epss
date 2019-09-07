bc8c3e67817a3c3885705fe990fb2409def27ab0
Firose <firumon@github.com>
2019-09-07 12:32:45 +0530

    Deleted unused component

--- app/components/DevMsg.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <TextTitle with="100%" class="text-center">Window under development</TextTitle>
-</template>
-
-<script>
-    export default {
-        name: "DevMsg"
-    }
-</script>
