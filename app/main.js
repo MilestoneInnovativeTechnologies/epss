d8e2f7e990a7a3359d0d5adc4bd198ae5b88a776
Firose <firumon@github.com>
2019-09-13 15:21:27 +0530

    Event Bus added

--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ import Home from './components/Home'
 import {TNSFontIcon, fonticon} from 'nativescript-fonticon';
 
 global._ = require('lodash'); global.moment = require('moment'); global.__ = {}; global.sql = require('sqlstring');
-global.DB = require('./assets/scripts/services/database').DB;
+global.DB = require('./assets/scripts/services/database').DB; global.EB = new Vue();
 
 import "./app.scss";
 import "./app.css";
@@ -11,10 +11,10 @@ import "./app.css";
 import store from './assets/scripts/vuex/store';
 global.log = function(text,...texts){ store.commit('Log/addQueue',text); if(TNS_ENV !== 'production') console.log(text,...texts); };
 
-// import VueDevtools from 'nativescript-vue-devtools'
-// if(TNS_ENV !== 'production') {
-//   Vue.use(VueDevtools,{ host:'192.168.10.11' })
-// }
+import VueDevtools from 'nativescript-vue-devtools'
+if(TNS_ENV !== 'production') {
+  Vue.use(VueDevtools,{ host:'192.168.10.11' })
+}
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = true;//(TNS_ENV === 'production');
