e987438cbc09359dc24f2bf252b0f69f9e2ac9b6
Firose <firumon@github.com>
2019-09-13 15:29:06 +0530

    Tax getter updated as per new modification

--- app/assets/scripts/vuex/modules/products/getters.js
+++ app/assets/scripts/vuex/modules/products/getters.js
@@ -5,7 +5,7 @@ export function productPrice(state,{ _tableDataFilter }) {
     return (product) => { return _(_tableDataFilter('pricelist','product',product)).groupBy('product').mapValues(PLArray => _.get(_.head(PLArray),'price')).get(product,0) }
 }
 export function productTax(state,{ _tableDataById }) {
-    let tfStr = 'taxfactor' + ((__.TAX02 === 'Yes') ? '02' : '');
+    let tfStr = 'taxfactor' + ((__.TAX02 === 'Yes') ? '02' : '01');
     let factors = [tfStr,'sub'+tfStr];
     return (product) => { return _.sum(_.values(_.only(_.get(_tableDataById('products'),product),factors))) }
 }
