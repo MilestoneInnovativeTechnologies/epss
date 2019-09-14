47d1786c9484f9a63f8c644bbd9c85d8b9630e6b
Firose <firumon@github.com>
2019-09-14 14:27:27 +0530

    Made effective for getting product id on list selection

--- app/components/pages/widescreen/sales/WSSaleContainer.vue
+++ app/components/pages/widescreen/sales/WSSaleContainer.vue
@@ -17,7 +17,11 @@
         } },
         computed: {
             ...mapGetters({ getProductsPerList:'Product/listProducts',getProductDetail:'Product/product' }),
-            productIds(){ return this.getProductsPerList(this.list01[0],this.list01[1],this.list02[0],this.list02[1]) },
+            productIds(){ return (this.list01.length)
+                ? (this.list02.length)
+                    ? this.getProductsPerList(this.list01[0],this.list01[1],this.list02[0],this.list02[1])
+                    : this.getProductsPerList(this.list01[0],this.list01[1],null,null)
+                : [] },
             productDetails(){ return _.map(this.productIds,(pid) => this.getProductDetail(pid)) },
             items(){ let filter = this.filter; return filter ? _.filter(this.productDetails,(product) => this.isProductIn(product,this.filter)) : this.productDetails }
         },
