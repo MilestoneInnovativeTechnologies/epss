aa234deb038c1395501d2ad79f0b347afab6b966
Firose <firumon@github.com>
2019-09-19 13:28:41 +0530

    Normal app template implemented in print preview

--- app/components/pages/preview/Preview.vue
+++ app/components/pages/preview/Preview.vue
@@ -1,27 +1,18 @@
 <template>
-    <Page actionBarHidden="true" backgroundSpanUnderStatusBar="false">
-        <GridLayout rows="*,60">
-            <ScrollView row="0">
-                <StackLayout width="95%">
-                    <PreviewHead class="m-b-15" @print="printHead = $event"></PreviewHead>
-                    <TextHeading v-if="title" style="width: 100%; text-align: center" class="m-b-15">{{ title }}</TextHeading>
-                    <slot></slot>
-                    <template v-if="template">
-                        <component v-for="(compAry,no) in template" :is="properComponent(compAry[0])" v-bind="compAry[1]" :key="['pt',unique,no].join('-')" class="m-b-15" @print="printCmd[no] = $event"></component>
-                    </template>
-                    <PreviewFooter class="m-t-20" @print="printFooter = $event"></PreviewFooter>
-                </StackLayout>
-            </ScrollView>
-            <StackLayout row="1" orientation="horizontal"><AppButton @tap.native="doPrint">PRINT</AppButton></StackLayout>
-        </GridLayout>
-    </Page>
+    <App title="Print Preview" action="Print" @print="doPrint">
+        <PreviewHead class="m-b-15" @print="printHead = $event"></PreviewHead>
+        <TextHeading v-if="title" style="width: 100%; text-align: center" class="m-b-15">{{ title }}</TextHeading>
+        <slot></slot>
+        <template v-if="template">
+            <component v-for="(compAry,no) in template" :is="properComponent(compAry[0])" v-bind="compAry[1]" :key="['pt',unique,no].join('-')" class="m-b-15" @print="printCmd[no] = $event"></component>
+        </template>
+        <PreviewFooter class="m-t-20" @print="printFooter = $event"></PreviewFooter>
+    </App>
 </template>
 
 <script>
-    import AppButton from "../../button/AppButton";
     export default {
         name: "Preview",
-        components: {AppButton},
         props: ['title','template'],
         data(){ return {
             printHead: [],
@@ -34,7 +25,7 @@
         },
         methods: {
             properComponent(comp){ return 'Preview' + _.upperFirst(comp); },
-            doPrint(){ print(this.printData) }
+            doPrint(){ print(this.printData); this.$navigateBack(); }
         },
         mounted(){
             let vm = this;
@@ -42,7 +33,7 @@
                 let printData = [].concat(printer.CENTERON(),vm.printHead,printer.LF(1),printer.CENTERON(),printer.HL());
                 printData = printData.concat(printer.LF(1),printer.CENTERON(),printer.TEXT(vm.title),printer.LF(2),printer.CENTEROFF());
                 printData = printData.concat(vm.printCmd.filter(array => array).flatMap(array => array.concat(printer.LF(1))).slice(0,-1));
-                printData = printData.concat(printer.LF(3),printer.CENTERON(),printer.HL(),printer.LF(1),printer.CENTERON(),vm.printFooter);
+                printData = printData.concat(printer.LF(2),printer.CENTERON(),printer.HL(),printer.LF(1),printer.CENTERON(),vm.printFooter);
                 vm.printData = printData;
             })
         }
