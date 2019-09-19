a5528ca616146e01de803cb53c70455021b9ada1
Firose <firumon@github.com>
2019-09-19 13:03:18 +0530

    In picker product name will be taken from narration

--- app/assets/scripts/mixins/formelement/product.js
+++ app/assets/scripts/mixins/formelement/product.js
@@ -5,7 +5,7 @@ export const FormElementMixinProduct = {
     computed: {
         ...mapState('Product',{ feProductList:'list' }),
         feValuesProduct(){
-            let list = this.feProductList, key = 'id', label = 'name', listArray = _.map(list,(product) => _.zipObject([key,label],[product[key],product[label]]));
+            let list = this.feProductList, key = 'id', label = 'narration', listArray = _.map(list,(product) => _.zipObject([key,label],[product[key],product[label]]));
             return { items:listArray, key, label };
         },
         feFieldProduct(){ return { name:'product',label:'Select Product',type:'Picker',values:this.feValuesProduct } }
