1ee85a126d5aafb0c0be495223a389fd5bda94ab
Firose <firumon@github.com>
2019-09-19 13:03:41 +0530

    Sales order items to sales mixin

--- app/assets/scripts/mixins/salesordertosaleitems.js
+++ app/assets/scripts/mixins/salesordertosaleitems.js
@@ -5,14 +5,14 @@ export const SalesOrderToSaleItems = {
         SOSIFNCode: null,
     } },
     computed:{
-        ...mapGetters({ SOSIGetSalesOrderItems:'SalesOrder/salesOrderItems',SOSIReferences:'_ref',SOSIGetProduct:'Product/product',SOSITotal:'total',SOSITax:'Product/tax' }),
+        ...mapGetters({ SOSIGetSalesOrderItems:'SalesOrder/salesOrderItems',SOSIReferences:'_ref',SOSIGetProduct:'Product/product',SOSITax:'Product/tax' }),
     },
     methods: {
         SOSI(id){
             let SOI = this.SOSIGetSalesOrderItems(id), _refs = this.SOSIReferences(SOI.length), SOSIItems = []; if(SOI.length < 2) _refs = [_refs];
             SOSIItems = _.map(SOI,(soi,idx) => {
                 let product = this.SOSIGetProduct(soi.product), tax = this.SOSITax(soi.product,this.SOSIFNCode);
-                return Object.assign({},product,{ _ref:_refs[idx],quantity:soi.quantity,rate:soi.rate,taxcode:tax[0],tax:tax[1],total:this.SOSITotal(soi.rate,soi.quantity,tax[1]) });
+                return Object.assign({},product,{ _ref:_refs[idx] },_.pick(soi,['quantity','rate','taxrule','tax','total']),{ taxfactor:tax[1] });
             });
             return SOSIItems;
         }
