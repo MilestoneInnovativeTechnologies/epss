a7e698ea2b39115b5040086509a0015a037cabc2
Firose <firumon@github.com>
2019-09-19 13:09:12 +0530

    Component to switch between wide screen and normal

---
+++ app/components/pages/sales/Sale.vue
@@ -0,0 +1,20 @@
+<template>
+    <WSSaleNew v-if="isWide"></WSSaleNew>
+    <SaleNew v-else></SaleNew>
+</template>
+
+<script>
+    import { mapState } from 'vuex';
+    import SaleNew from "./SaleNew";
+    export default {
+        name: "Sale",
+        components: {SaleNew},
+        data(){ return {
+            wideMinWidth: 900,
+        } },
+        computed: {
+            ...mapState('App',['width']),
+            isWide(){ let width = this.width; return (this.wideMinWidth < _.toNumber(width)) }
+        }
+    }
+</script>
