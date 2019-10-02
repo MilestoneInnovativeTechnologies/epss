8b775948180906f9809019bd089afa0ffbadac89
Firose <firumon@github.com>
2019-10-02 16:48:44 +0530

    Print module in external file

--- app/main.js
+++ app/main.js
@@ -1,19 +1,10 @@
 import Vue from 'nativescript-vue'
 import Home from './components/Home'
 import {TNSFontIcon, fonticon} from 'nativescript-fonticon';
-import { Printer } from './assets/scripts/services/Printer';
 
 global._ = require('lodash'); global.moment = require('moment'); global.__ = {}; global.sql = require('sqlstring');
 global.DB = require('./assets/scripts/services/database').DB; global.EB = new Vue();
-global.printer = new Printer(); global.print = function(data){
-    if(arguments.length){
-        let cmd = printer.INIT(); for(let i = 0; i < arguments.length; i++){
-            if(Array.isArray(arguments[i])) cmd = cmd.concat(arguments[i]);
-            else cmd = cmd.concat(printer.TEXT(arguments[i]));
-        }
-        printer.print(cmd.concat(Array(4).fill(10)));
-    }
-};
+global.print = require('./assets/scripts/services/Printer.js').print;
 
 import "./app.scss";
 import "./app.css";
