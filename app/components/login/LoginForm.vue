cd36e38b4839718768880642d8eacf68793febd8
Firose <firumon@github.com>
2019-10-02 19:30:25 +0530

    Email changed to Login

--- app/components/login/LoginForm.vue
+++ app/components/login/LoginForm.vue
@@ -1,8 +1,8 @@
 <template>
     <AbsoluteLayout>
         <StackLayout width="100%">
-            <TextRegular class="cp">Email</TextRegular>
-            <TextField v-model="email" hint="Email" class="cp fsi" @returnPress="focusPassword"></TextField>
+            <TextRegular class="cp">Login</TextRegular>
+            <TextField v-model="login" hint="Login" class="cp fsi" @returnPress="focusPassword"></TextField>
             <TextRegular class="cp m-t-10">Password</TextRegular>
             <TextField v-model="password" hint="Password" class="cp fsi" secure="true" ref="password"></TextField>
         </StackLayout>
@@ -20,16 +20,12 @@
         name: "LoginForm",
         computed:{
             ...mapState('User',['validating']), ...mapGetters('User',['credentials']),
-            email:{ get(){ return this.credentials.email },  set(val){ this[set_state_data](_.set({},'email',val)) } },
+            login:{ get(){ return this.credentials.login },  set(val){ this[set_state_data](_.set({},'login',val)) } },
             password:{ get(){ return this.credentials.password },  set(val){ this[set_state_data](_.set({},'password',val)) } },
         },
         methods: {
             ...mapMutations('User',[set_state_data]),
             focusPassword(){ this.$refs.password.nativeView.focus() },
-        },
-        mounted(){
-            this.email = 'SE01.sls@temp.mail';
-            this.password = '123456';
         },
     }
 </script>
