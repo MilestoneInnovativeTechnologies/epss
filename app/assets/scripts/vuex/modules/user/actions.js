942fdc1f51ee8f84e59bc867c1ea358477c19e39
Firose <firumon@github.com>
2019-10-17 19:28:32 +0530

    Implemented login, reset of lists

--- app/assets/scripts/vuex/modules/user/actions.js
+++ app/assets/scripts/vuex/modules/user/actions.js
@@ -1,6 +1,7 @@
 import {add_event_subscriber, remove_user_tables_from_sync, set_state_data} from "../../mutation-types";
 
 const userStateKeys = ['name','email','id','api_token','reference'];
+const makeModuleListNull = ['Stores','Areas','Customer','SalesOrder'];
 
 export function doLogin({ state,commit,dispatch }){
     let { login,password } = state; commit(set_state_data,{ validating:true });
@@ -9,6 +10,7 @@ export function doLogin({ state,commit,dispatch }){
 
 export function loginResponse({ commit,dispatch,state }, data) {
     if(_.isEmpty(data)) return commit(set_state_data,{ validating:false,message:'No email and password matching records found' });
+    makeModuleListNull.forEach((mod) => commit(mod+'/'+set_state_data,{ list:[] },{ root:true }));
     if(data.login != state.pLogin) dispatch('Sync/forceDownloadUserTables',null,{ root:true });
     dispatch('doInsertLoginData',getNameDetailArray(data));
 }
