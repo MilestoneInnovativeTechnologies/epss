cfa4ff46c4b771bebaf6ed29255eb7130c67febc
Firose <firumon@github.com>
2019-10-17 18:44:27 +0530

    ePlus password encoding notification added

--- app/components/login/LoginForm.vue
+++ app/components/login/LoginForm.vue
@@ -1,10 +1,10 @@
 <template>
     <AbsoluteLayout>
         <StackLayout width="100%">
-            <TextRegular class="cp">Login</TextRegular>
-            <TextField v-model="login" hint="Login" class="cp fsi" @returnPress="focusPassword"></TextField>
-            <TextRegular class="cp m-t-10">Password</TextRegular>
-            <TextField v-model="password" hint="Password" class="cp fsi" secure="true" ref="password"></TextField>
+            <StackLayout v-if="encNotOk" width="80%" class="p-30 text-center" style="color: #FF0000; border-color: #FF0000; border-width: 2">
+                <Label :textWrap="true" text="It seems that your ePlus Password Encoding is not configured as UTF8. Login won't work for other encoding types, So please make your ePlus Password Encoding to UTF8 and do at least one login in ePlus.!" />
+            </StackLayout>
+            <AppForm title="Login" :fields="fields()" :values="values" @final="updateCredentials"></AppForm>
         </StackLayout>
         <StackLayout width="100%" height="175">
             <ActivityIndicator :busy="validating" class="m-t-30"></ActivityIndicator>
@@ -13,19 +13,22 @@
 </template>
 
 <script>
-    import {set_state_data} from '../../assets/scripts/vuex/mutation-types'
+    import {set_state_data } from '../../assets/scripts/vuex/mutation-types'
     import { mapState,mapGetters,mapMutations } from 'vuex';
+    const feMX = require('./../../assets/scripts/mixins/formelement');
 
     export default {
         name: "LoginForm",
+        mixins: [feMX.common,feMX.loginusers,feMX.password],
         computed:{
             ...mapState('User',['validating']), ...mapGetters('User',['credentials']),
-            login:{ get(){ return this.credentials.login },  set(val){ this[set_state_data](_.set({},'login',val)) } },
-            password:{ get(){ return this.credentials.password },  set(val){ this[set_state_data](_.set({},'password',val)) } },
+            values(){ return { login:this.credentials.login,password:this.credentials.password } },
+            encNotOk(){ return __.PASSWORDENCODING !== 'Utf8' },
         },
         methods: {
             ...mapMutations('User',[set_state_data]),
-            focusPassword(){ this.$refs.password.nativeView.focus() },
+            fields(){ return this.appFormFields({ login:'LoginUsers',password:'Password' }) },
+            updateCredentials(data){ let vm = this; _.forEach(data,(value,field) => vm[set_state_data](_.set({},field,value))) },
         },
     }
 </script>
