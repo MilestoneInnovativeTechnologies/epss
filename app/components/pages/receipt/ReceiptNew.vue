5ac87b1d18b4f6f518060667136e06449c28cd17
Firose <firumon@github.com>
2019-10-17 18:50:01 +0530

    Redesigned

--- app/components/pages/receipt/ReceiptNew.vue
+++ app/components/pages/receipt/ReceiptNew.vue
@@ -1,46 +1,79 @@
 <template>
-    <App title="New Receipt" action="Save" @save="save">
-        <AppForm :fields="receiptAppFields" :values="values" @mode="mode = $event" @final="setReceiptFields"></AppForm>
-        <StackLayout v-if="mode === 'Cheque'">
-            <AppForm :fields="chequeAppFields" @final="setReceiptFields"></AppForm>
-        </StackLayout>
+    <App :title="title" action="Save" @save="saveReceipt">
+        <AppForm :fields="fields" :values="values" @final="updateData"></AppForm>
     </App>
 </template>
 
 <script>
     import { mapGetters,mapActions } from 'vuex';
-    import {fetch_all_active_receipts} from "../../../assets/scripts/queries";
+    import {ReceiptNew,PrintModal} from "../../../assets/scripts/navigations";
     const feMX = require('./../../../assets/scripts/mixins/formelement');
+    const fields = { customer:'Customer',date:'DatePicker',amount:'Amount',bank:'Text',cheque:'Text',cheque_date:'DatePicker' };
+    const ThisOBJ = require('./../../../assets/scripts/mixins/tobj').ThisObj;
+    const FNFields = {
+        'CR1': ['date','customer','amount'],
+        'BR1': ['date','customer','bank','cheque','cheque_date','amount'],
+        'BR2': ['date','customer','amount'],
+    };
+    const LabelCorrection = { bank:'Bank Name',cheque:'Cheque Number',cheque_date:'Cheque Date' };
+    const saveFields = ['docno','store','fycode','fncode','user','customer','date','amount','bank','cheque_date','cheque','source','_ref'];
+
+
+
+
+    const template = [
+        { type:'raw',source:'receipts',keys:{ docno:'Document',date:'Date',customer_name:'Customer' } },
+        { type:'line' },
+        { type:'raw',source:'receipts',keys:{ amount:'Amount' } },
+        { type:'line' },
+        { type:'raw',source:'receipts',keys:{ cheque:'Cheque Number',cheque_date:'Cheque Date',bank:'Bank Name', } }
+    ];
+
+
+
+
 
     export default {
         name: "ReceiptNew",
-        mixins: [feMX.common, feMX.customer, feMX.paymentmode, feMX.store, feMX.fiscal, feMX.datepicker, feMX.amount, feMX.text],
-        props: ['pMode'],
+        mixins: [ThisOBJ,feMX.common, feMX.customer, feMX.paymentmode, feMX.store, feMX.fiscal, feMX.datepicker, feMX.amount, feMX.text],
+        props: ['fncode','fycode','store','title'],
         data(){ return {
-            receiptFields: { customer:'Customer',mode:'PaymentMode',store:'Store',fycode:'Fiscal',date:'DatePicker',amount:'Amount' },
-            chequeFields: { bank:'Text',cheque:'Text',cheque_date:'DatePicker' },
-            mode: 'Cash', final: {},
-        }},
+            eDocno: null,
+            customer: null, date: null, amount: null,
+            bank: null, cheque_date: null, cheque: null, source: 'SS',
+        } },
         computed: {
-            ...mapGetters({ date:'date' }),
-            values(){ return { date:this.date(),mode:this.mode } },
-            receiptAppFields(){ return this.appFormFields(this.receiptFields) },
-            chequeAppFields(){ let fields = this.appFormFields(this.chequeFields); return _.mapValues(fields,(field,name) => { field.label = _.startCase(name); return field; }) },
+            ...mapGetters({ curDate:'date',user:'user',nRef:'_ref',toDateTime:'toDateTime' }),
+            ...mapGetters('Reserves',{ getDocNO:'get' }),
+            ...mapGetters({ customerDetail:'Customer/customer' }),
+
+            values(){ return { date:this.curDate() } },
+            fields(){ return this.correctLabels(this.appFormFields(_.pick(fields,FNFields[this.fncode]))) },
+            docno(){ return () => this.getDocNO(this.store,this.fycode,this.fncode); },
+            _ref(){ return this.nRef(); },
+            reloadProps(){ return this.TO_Get(['store','fycode','fncode','title']) }
         },
         methods: {
-            ...mapActions('Receipts',{ newReceipt:'create',stock:'_stock' }),
-            setReceiptFields(data){ this.final = Object.assign({},this.final,data) },
-            save(){
-                if(_.isEmpty(this.final.amount) && !_.isNumber(this.final.amount)) return alert('Please enter any amount');
-                this.newReceipt(this.final).then(id => {
-                    this.stock({ query:sql.format(fetch_all_active_receipts) }).then(() => {
-                        this.$navigateTo(require('./ReceiptDetail').default,{ props:{ id }})
-                    });
-                });
-            }
+            ...mapActions('Receipts',{ saveReceiptData:'save' }),
+            correctLabels(Obj) {
+                let cObj = Obj;
+                _.intersection(_.keys(Obj), _.keys(LabelCorrection))
+                    .map(field => cObj[field]['label'] = LabelCorrection[field]);
+                return cObj;
+            },
+            updateData(data){ let hData = Object.assign({},data,{ date:this.toDateTime(data.date) }); this.TO_SetPropFromObj(hData); },
+            saveReceipt(){
+                let data = this.TO_Get(saveFields);
+                this.saveReceiptData(data).then(ref => this.$navigateTo(ReceiptNew,{ props:this.reloadProps }))
+                    .then(ref => {
+                        let receipts = _.set(data,'customer_name',_.get(this.customerDetail(data.customer),'name'));
+                        this.$showModal(PrintModal,{ props: { title:this.title,data:{ receipts },template },fullscreen:true })
+                            .then(print_data => this.$navigateTo(ReceiptNew,{ props:this.reloadProps }))
+                    })
+            },
         },
-        created() {
-            this.mode = this.pMode || 'Cash'
+        created(){
+            setTimeout(() => this.eDocno = this.docno(),4000)
         }
     }
 </script>
--- app/components/pages/receipt/widgets/ReceiptList.vue
+++ app/components/pages/receipt/widgets/ReceiptList.vue
@@ -3,9 +3,6 @@
 </template>
 
 <script>
-    import { mapActions } from 'vuex';
-    import {fetch_all_active_receipts} from "../../../../assets/scripts/queries";
-
     export default {
         name: "ReceiptList",
         props: ['receipts','layout','cast','detail','links','title','limit'],
@@ -20,10 +17,6 @@
             listCast(){ return this.cast || this.defaultCast },
             listDetail(){ return this.detail || this.defaultDetail },
             listLinks(){ return this.links || this.defaultLinks },
-        },
-        methods: mapActions('Receipts',['_stockIfNot']),
-        created() {
-            this._stockIfNot({ query:sql.format(fetch_all_active_receipts) })
         }
     }
 </script>
