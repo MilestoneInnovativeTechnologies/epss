e04728634071a4e5e29cb1574f07e9e513c93c78
Firose <firumon@github.com>
2019-10-17 19:06:17 +0530

    Navigation contents

---
+++ app/assets/scripts/navigations.js
@@ -0,0 +1,13 @@
+export const ReceiptNew = require('../../components/pages/receipt/ReceiptNew').default;
+
+export const NewSaleTransaction = require('../../components/pages/transaction/NewSaleTransaction').default;
+export const NewSaleTransactionAdvanced = require('../../components/pages/transaction/NewSaleTransactionAdvanced').default;
+
+export const NewSalesOrder = require('../../components/pages/salesorder/NewSalesOrder').default;
+export const NewSalesOrderAdvanced = require('../../components/pages/salesorder/NewSalesOrderAdvanced').default;
+
+
+export const PrintModal = require('../../components/pages/temp/PrintModal').default;
+
+
+
