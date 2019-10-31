a65b5c3049b29ee121f596acbab404b6737fdf0b
Firose <firumon@github.com>
2019-10-31 15:47:12 +0530

    New navigations added

--- app/assets/scripts/navigations.js
+++ app/assets/scripts/navigations.js
@@ -1,3 +1,6 @@
+export const Home = require('../../components/Home').default;
+export const MaterialTransferIndex = require('../../components/index/MaterialTransferIndex').default;
+
 export const ReceiptNew = require('../../components/pages/receipt/ReceiptNew').default;
 
 export const NewSaleTransaction = require('../../components/pages/transaction/NewSaleTransaction').default;
@@ -6,8 +9,6 @@ export const NewSaleTransactionAdvanced = require('../../components/pages/transa
 export const NewSalesOrder = require('../../components/pages/salesorder/NewSalesOrder').default;
 export const NewSalesOrderAdvanced = require('../../components/pages/salesorder/NewSalesOrderAdvanced').default;
 
+export const StockTransferInItems = require('../../components/pages/transfer/StockTransferInItems').default;
 
 export const PrintModal = require('../../components/pages/temp/PrintModal').default;
-
-
-
