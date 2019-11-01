ba40f43a60cffc30387ccf80f81e36441d77c417
Firose <firumon@github.com>
2019-11-01 17:36:08 +0530

    Global variable CCache for DBCache storage access

--- app/main.js
+++ app/main.js
@@ -5,15 +5,16 @@ import {TNSFontIcon, fonticon} from 'nativescript-fonticon';
 
 global._ = require('lodash'); global.moment = require('moment'); global.__ = {}; global.sql = require('sqlstring');
 global.DB = require('./assets/scripts/services/database').DB; global.EB = require('./assets/scripts/services/EventBus').EventBus;
-global.print = require('./assets/scripts/services/Printer.js').print; global.DBCache = require('./assets/scripts/services/DBCache').DBCache;
+global.print = require('./assets/scripts/services/Printer.js').print;
+global.DBCache = require('./assets/scripts/services/DBCache').DBCache; global.CCache = {};
 
 global.VuexStore = require('./assets/scripts/vuex/store').default;
 global.log = function(text,...texts){ VuexStore.commit('Log/addQueue',text); if(TNS_ENV !== 'production') console.log(text,...texts); };
 
-// import VueDevtools from 'nativescript-vue-devtools'
-// if(TNS_ENV !== 'production') {
-//   Vue.use(VueDevtools,{ host:'192.168.10.11' })
-// }
+import VueDevtools from 'nativescript-vue-devtools'
+if(TNS_ENV !== 'production') {
+  Vue.use(VueDevtools,{ host:'192.168.10.11' })
+}
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production');
