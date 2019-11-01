5347e1e7790d9ae3e2ead68ce64d13a4102bd3e6
Firose <firumon@github.com>
2019-11-01 20:20:18 +0530

    Form element updated to take products from cache file

--- app/assets/scripts/mixins/formelement/product.js
+++ app/assets/scripts/mixins/formelement/product.js
@@ -1,16 +1,15 @@
-import { mapState,mapActions } from "vuex";
+import { mapActions } from "vuex";
 
 export const FormElementMixinProduct = {
     computed: {
-        ...mapState('Product',{ feProductList:'dbData' }),
         feValuesProduct(){
-            let list = this.feProductList['products'], key = 'id', label = 'narration', listArray = _.map(list,(product) => _.zipObject([key,label],[product[key],product[label]]));
+            let list = CCache['products'].all(), key = 'id', label = 'narration', listArray = _.map(list,(product) => _.zipObject([key,label],[product[key],product[label]]));
             return { items:listArray, key, label };
         },
         feFieldProduct(){ return { name:'product',label:'Select Product',type:'Picker',values:this.feValuesProduct } }
     },
     methods: mapActions(['redrawModules']),
     created(){
-        if(this.feProductList['products'].length === 0) this.redrawModules('products');
+        if(!CCache['products']) this.redrawModules('products');
     }
 };
