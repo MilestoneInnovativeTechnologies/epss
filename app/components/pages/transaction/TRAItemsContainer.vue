1fc02cb4c41a031749a1c2ec41cc3fda5a9ae948
Firose <firumon@github.com>
2019-11-01 20:49:49 +0530

    Products from cache file

--- app/components/pages/transaction/TRAItemsContainer.vue
+++ app/components/pages/transaction/TRAItemsContainer.vue
@@ -11,7 +11,7 @@
 <script>
     import { mapGetters } from 'vuex';
     import {EventListeners} from "../../../assets/scripts/mixins/eventlisteners";
-    let products = {};
+    let products = CCache['products'].dataById();
 
     export default {
         name: "TRAItemsContainer",
@@ -20,10 +20,9 @@
         data(){ return {
             filter: '', list01: 0, list02: 0, curPage: 1, update: 0,
             events: ['tra-list01-changed','tra-list02-changed'],
-            pReady: false
         } },
         computed: {
-            ...mapGetters({ getProductsPerList:'Product/listProducts',getProductDetail:'Product/product',allProducts:'Product/_tableDataById' }),
+            ...mapGetters({ getProductsPerList:'Product/listProducts',getProductDetail:'Product/product' }),
 
             productIds(){
                 return (this.list1 && this.list01)
@@ -32,8 +31,8 @@
                         : this.getProductsPerList(this.list1,this.list01,null,null)
                     : [];
             },
-            productDetails(){ return _.map(this.productIds,(pid) => (this.pReady) ? products[pid] : this.getProductDetail(pid)); },
-            fewProductDetails(){ return _.map(this.productIds.slice(0,this.perPage*3),(pid) => (this.pReady) ? products[pid] : this.getProductDetail(pid)); },
+            productDetails(){ return _.map(this.productIds,(pid) => products[pid]) },
+            fewProductDetails(){ return _.map(this.productIds.slice(0,this.perPage*3),(pid) =>  products[pid]); },
             filteredItems(){ let filter = this.filter; return filter ? _.filter(this.productDetails,(product) => isProductIn(product,filter)) : this.fewProductDetails; },
             perPage(){ return _.toSafeInteger(this.properties.itemsPerPage); },
             items(){ let items = this.filteredItems, start = (this.curPage-1) * this.perPage, end = start + this.perPage; return items.slice(start,end); },
@@ -43,9 +42,6 @@
             filterText(text){ this.curPage = 1; this.filter = text; },
             listener0(data){ this.list01 = data; this.curPage = 1; },
             listener1(data){ this.list02 = data; this.curPage = 1; },
-        },
-        mounted(){
-            if(!this.pReady) setTimeout(() => { products = this.allProducts('products'); this.pReady = true; },3000);
         }
     }
 
