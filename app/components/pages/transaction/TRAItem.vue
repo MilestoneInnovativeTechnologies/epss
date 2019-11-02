904b1ee891d4eb167b9ef8d4dcdfdf4e86b09acd
Firose <firumon@github.com>
2019-11-02 21:57:57 +0530

    Layout changed and caching image made little more effective

--- app/components/pages/transaction/TRAItem.vue
+++ app/components/pages/transaction/TRAItem.vue
@@ -1,25 +1,54 @@
 <template>
-    <StackLayout :width="width" :height="height" style="border-width: 2" class="bordercg01 p-t-2" @tap="addItem(item)">
-        <TRAItemImage :item="item" :width="width" :height="width"></TRAItemImage>
-        <FlexboxLayout :height="height-width" class="bcp">
-            <Label :text="name" class="c-white fs10 fsb p-5" :width="width - (height-width) - 5" textWrap="true"></Label>
-            <StackLayout verticalAlignment="middle" class="bcp m-l-5 bordercp" style="border-width: 2; background-color: #FFFFFF">
-                <Label :width="height-width" :text="rate" class="fsb fs14 text-center cp c-bg-white"></Label>
-            </StackLayout>
-        </FlexboxLayout>
-    </StackLayout>
+    <GridLayout :width="width" :height="height" ref="item" :rows="rows" :columns="cols" class="p-t-2 item-grid" @tap="addItem(item)">
+        <Image row="0" col="0" colSpan="2" rowSpan="2" :src="src" loadMode="async" stretch="aspectFill" useCache="true" />
+        <Label row="1" col="0" :text="name" class="fsb p-5 label-grid cp" :class="labelFontSize" textWrap="true"></Label>
+        <StackLayout row="1" col="1" :height="priceLength[1]" verticalAlignment="middle" style="background-color: #FFFFFF">
+            <Label :width="priceLength[0]" :text="rate" class="fsb fs18 text-center cp c-bg-white"></Label>
+        </StackLayout>
+    </GridLayout>
 </template>
 
 <script>
+    import { mapGetters } from 'vuex';
+    const { fromNativeSource } = require("tns-core-modules/image-source");
+
     export default {
         name: "TRAItem",
-        props: ['item','width','height'],
+        props: ['item','width','height','priceWidth'],
+        data(){
+            let spanLength = this.width, labelLength = [this.width - this.priceWidth - 5,this.height - this.width], priceLength = [this.priceWidth,this.height - this.width],
+                rows = [spanLength,labelLength[1]].join(','), cols = [labelLength[0],priceLength[0]].join(',');
+            return { spanLength, labelLength, priceLength, rows, cols, src:null }
+        },
         computed: {
+            ...mapGetters('Product',['imagePath']),
             name(){ return this.item.narration },
             rate(){ return __.rate(this.item.price) },
+            labelFontSize(){ return (this.name.toString().length > 60) ? ['fs14'] : ['fs16']},
+            imageURL(){ return [this.imagePath,this.item.id].join('') },
+            cacheKey(){ return 'item-image-' + this.item.id }
         },
         methods: {
             addItem(item){ EB.$emit('tra-item-selected',this.item) }
+        },
+        created(){
+            let imgSrc = ImageCache.get(this.cacheKey);
+            if(imgSrc) return this.src = fromNativeSource(imgSrc);
+            this.src = ImageCache.placeholder;
+            let url = this.imageURL, key = this.cacheKey, vm = this;
+            ImageCache.enqueue({ key,url,completed(image){ vm.src = fromNativeSource(image) } })
         }
     }
 </script>
+
+<style>
+    .item-grid {
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-size: contain;
+    }
+    .label-grid {
+        background-image: url("~/assets/images/t80.png");
+        background-repeat: repeat;
+    }
+</style>
