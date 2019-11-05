a6f0f59b3d823c284fcea9ec12fb3059297ae282
Firose <firumon@github.com>
2019-11-05 21:41:59 +0530

    Rearranged

--- app/assets/scripts/debugs.js
+++ app/assets/scripts/debugs.js
@@ -1,8 +1,7 @@
 import Vue from 'nativescript-vue';
 import VueDevtools from 'nativescript-vue-devtools'
-if(TNS_ENV !== 'production') {
-  Vue.use(VueDevtools,{ host:'192.168.10.11' })
-}
 
-// Prints Vue logs when --env.production is *NOT* set while building
-Vue.config.silent = (TNS_ENV === 'production');
+if(TNS_ENV !== 'production') {
+  Vue.use(VueDevtools,{ host:'192.168.10.11' });
+  Vue.config.silent = false;
+}
