c1d1aabe8b135dcd357702f8a9cf7882c761cf00
Firose <firumon@github.com>
2019-11-07 13:10:29 +0530

    Debug lines removed

--- app/assets/scripts/vuex/modules/download/actions.js
+++ app/assets/scripts/vuex/modules/download/actions.js
@@ -6,20 +6,17 @@ let timeOut = 0;
 const tableID = {};
 
 export function tables({ dispatch },tables){
-    console.error('Called action Download/tables with tables: ',tables);
     if(!Array.isArray(tables) || tables.length === 0) return null;
     dispatch('batch',tables).then(() => dispatch('processBatch'));
 }
 
 export function batch({ state,commit },tables){
-    console.error('Called action Download/batch with tables: ',tables);
     tables.forEach(table => state.batch.running.includes(table) ? null : commit('addToBatch',table));
     return state.batch;
 }
 
-export function processBatch({ state,commit,dispatch }){
-    console.error('Called action Download/processBatch');
-    if(state.processing) { clearTimeout(timeOut); return timeOut = setTimeout(() => dispatch('processBatch'),5000) }
+export function processBatch({ state,commit,dispatch,rootState }){
+    if(state.processing || !rootState['Connection'].status) { clearTimeout(timeOut); return timeOut = setTimeout(() => dispatch('processBatch'),5000) }
     if(state.batch.next.length){
         commit('start'); dispatch('triggerEventSubscribers',{ event:state.subscribeEvents[0],payload:state.batch.running },{ root:true });
         state.batch.running.forEach(table => dispatch('table',table));
@@ -28,19 +25,13 @@ export function processBatch({ state,commit,dispatch }){
 }
 
 export function table({ rootState,rootGetters },table){
-    console.error('Called action Download/table with table: ',table);
-    console.error('Table Properties: ',rootState['Sync'].tables[table]);
     let { type,direction } = rootState['Sync'].tables[table];
-    console.error('Type and Direction of table: '+table+' is ' + type + ', ' + direction);
-    if(direction === 'upload') return console.error('Direction of table: '+table+' is ' + direction + 'Cant go further');
+    if(direction === 'upload') return;
     let downloadData = getDownloadData(table,type,rootGetters);
-    console.warn('Prepared download data for table: '+table+', is:',downloadData);
     _.set(tableID,table,Downloader.createDownload(downloadData));
-    console.error('Set download task id for table: '+table+', and task id:'+tableID[table]);
 }
 
 export function start(ctx) {
-    console.error('In Dispatch DOWNLOAD/START.. Batch is:',ctx.state.batch,' and tableID: ',tableID);
     _.forEach(ctx.state.batch.running,(table,idx) => {
         let taskID = tableID[table]; if(!taskID) return;
         let success = _.bind(downloaded,ctx,table,taskID), fail = _.bind(download_fails,ctx,table,taskID);
@@ -51,7 +42,6 @@ export function start(ctx) {
 }
 
 export function SSEMonitor({ dispatch },tables) {
-    console.error('Called action Download/SSEMonitor with tables: ',tables);
     tables = Array.isArray(tables) ? tables : [tables]; if(tables.length === 0) return null;
     dispatch('tables',tables);
 }
@@ -84,26 +74,22 @@ function getName(table){
 }
 
 function downloaded(table,task,{ path }){
-    console.error('Download succeeded for table: '+table+'... File saved to path: ' + path);
     fsm.File.fromPath(path).readText()
         .then(activities => {
-            console.error('File read succeeded for table: '+table);
             if(activities && activities.trim() !== ''){
                 try {
                     let Activities = JSON.parse(activities);
                     this.dispatch('Sync/doProcessSyncData',Activities,{ root:true });
                     fsm.File.fromPath(path).remove().then(null);
-                    console.error('Removed file from path ' + path);
                 } catch (e) {
-                    console.error('Error in parsing activities of table: ' + table + '.. Path: ' + path);
+                    log('Error in parsing activities of table: ' + table + '.. Path: ' + path);
                 }
             }
             this.commit('complete',table);
         })
-        .catch(error => console.error('File read failed for table: '+table,error))
+        .catch(error => log('File read failed for table: '+table,error))
 }
 function download_fails(table,task,{ status,message }){
-    console.error('Download fails for table: '+table+'... Server response: ' + status +'.. Response Message: ' + message);
     this.dispatch('tables',[table]);
     this.commit('complete',table)
 }
--- app/assets/scripts/vuex/modules/download/mutations.js
+++ app/assets/scripts/vuex/modules/download/mutations.js
@@ -1,17 +1,14 @@
 export default {
     addToBatch(state,table){
-        console.error('Called mutation Download/addToBatch with table: ',table);
         if(!state.batch.running.concat(state.batch.next).includes(table))
             state.batch.next.push(table)
     },
     start(state){
-        console.error('Called mutation Download/start having batch:',state.batch);
         state.batch.running.push(...(state.batch.next));
         state.batch.next = [];
         state.processing = !!state.batch.running.length
     },
     complete(state,table){
-        console.error('Called action Download/complete of table: ',table);
         if(state.batch.running.includes(table))
             state.batch.running.splice(state.batch.running.indexOf(table),1);
         state.processing = !!state.batch.running.length;
