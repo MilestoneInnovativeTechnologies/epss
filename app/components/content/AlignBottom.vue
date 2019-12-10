1788eb572b0e94a31df7a0060d4c7e2536f6296a
Firose <firumon@github.com>
2019-12-10 14:17:14 +0530

    New component to make contents align on bottom of window

---
+++ app/components/content/AlignBottom.vue
@@ -0,0 +1,12 @@
+<template>
+    <GridLayout rows="*,auto" columns="*">
+        <StackLayout row="0" col="0"></StackLayout>
+        <StackLayout row="1" col="0"><slot></slot></StackLayout>
+    </GridLayout>
+</template>
+
+<script>
+    export default {
+        name: "AlignBottom"
+    }
+</script>
