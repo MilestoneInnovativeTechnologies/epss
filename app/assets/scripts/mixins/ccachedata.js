eaab1b14d6a8ad0e2e68d3e77850750efa2cfa28
Firose <firumon@github.com>
2019-12-13 18:13:08 +0530

    Get attribute also introduced as to do getting while preparing

--- app/assets/scripts/mixins/ccachedata.js
+++ app/assets/scripts/mixins/ccachedata.js
@@ -5,16 +5,17 @@ export const CCacheDataMixin = {
         CCacheDataTimeOut: 0,
     } },
     methods: {
-        CCacheDataRefill(table,key,method,args){
+        CCacheDataRefill(table,key,method,args,get){
             clearTimeout(this.CCacheDataTimeOut);
-            if(!CCache.hasOwnProperty(table)) return this.CCacheDataTimeOut = setTimeout(this.CCacheDataRefill,500,table,key,method,args);
-            this[key] = CCache[table][method](...args);
+            if(!CCache.hasOwnProperty(table)) return this.CCacheDataTimeOut = setTimeout(this.CCacheDataRefill,500,table,key,method,args,get);
+            let data = CCache[table][method](...args);
+            this[key] = get ? _.get(data,get) : data;
             this.CCacheDataReady = true;
         },
         CCacheDataPrepare(arg){
-            let { method,table,key,args } = this.CCacheDataCorrectArg(arg);
+            let { method,table,key,args,get } = this.CCacheDataCorrectArg(arg);
             if(!CCache.hasOwnProperty(table)) this.$store.dispatch('redrawModules',table);
-            this.CCacheDataRefill(table,key,method,args)
+            this.CCacheDataRefill(table,key,method,args,get)
         },
         CCacheDataCorrectArg(arg){
             arg = _.isObject(arg) ? arg : { table:arg };
@@ -22,6 +23,7 @@ export const CCacheDataMixin = {
             if(!arg.hasOwnProperty('key')) arg['key'] = arg['table'];
             if(!arg.hasOwnProperty('args')) arg['args'] = [];
             if(!Array.isArray(arg['args'])) arg['args'] = [arg['args']];
+            if(!arg.hasOwnProperty('get')) arg['get'] = null;
             return arg;
         }
     }
