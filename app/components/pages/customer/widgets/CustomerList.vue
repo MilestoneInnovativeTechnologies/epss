49d74568740c531f5065aac20cac45b2f0e3f761
Firose <firumon@github.com>
2019-12-13 18:15:10 +0530

    Widget to list customers created

---
+++ app/components/pages/customer/widgets/CustomerList.vue
@@ -0,0 +1,22 @@
+<template>
+    <AppList :source="customers" :layout="listLayout" :cast="listCast" :detail="listDetail" :links="listLinks"  :title="title || 'Customers'" :limit="limit || 10"></AppList>
+</template>
+
+<script>
+    export default {
+        name: "CustomerList",
+        props: ['customers','layout','cast','detail','links','title','limit'],
+        data(){ return {
+            defaultLayout: { Name:'name',Phone:'phone',Email:'email',Code:'code','Out Standing':'outstanding',Overdue:'overdue' },
+            defaultCast: { outstanding:'amount',overdue:'amount' },
+            defaultDetail: 'customer/CustomerDetail',
+            defaultLinks: {  },
+        }},
+        computed: {
+            listLayout(){ return this.layout || this.defaultLayout },
+            listCast(){ return this.cast || this.defaultCast },
+            listDetail(){ return this.detail || this.defaultDetail },
+            listLinks(){  },
+        }
+    }
+</script>
