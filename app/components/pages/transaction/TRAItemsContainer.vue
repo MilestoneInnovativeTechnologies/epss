f74d2096da104a96f0ef382a5c642419bf87b509
Firose <firumon@github.com>
2019-12-16 15:25:02 +0530

    Cache data mixin utilized for products

--- app/components/pages/transaction/TRAItemsContainer.vue
+++ app/components/pages/transaction/TRAItemsContainer.vue
@@ -1,7 +1,7 @@
 <template>
     <GridLayout rows="auto,*,auto" columns="*" :class="'bcg01 p-x-'+properties.containerPadding">
         <TRAItemsFilter col="0" row="0" @filter="filterText"></TRAItemsFilter>
-        <TRAItems :properties="properties" col="0" row="1" v-if="items.length" :items="items"></TRAItems>
+        <TRAItems :properties="properties" col="0" row="1" v-if="CCacheDataReady && items.length" :items="items"></TRAItems>
         <HorizontallyMiddle col="0" row="2">
             <TRAItemsPagination :totPage="TotalPages" :curPage="curPage" @change-page-to="curPage = $event === '...' ? curPage : $event" class="m-t-5 bordercp" style="border-top: 1"></TRAItemsPagination>
         </HorizontallyMiddle>
@@ -11,11 +11,11 @@
 <script>
     import { mapGetters } from 'vuex';
     import {EventListeners} from "../../../assets/scripts/mixins/eventlisteners";
-    let products = {};
+    import {CCacheDataMixin} from "../../../assets/scripts/mixins/ccachedata";
 
     export default {
         name: "TRAItemsContainer",
-        mixins: [EventListeners],
+        mixins: [EventListeners,CCacheDataMixin],
         props: ['properties','list1','list2'],
         data(){ return {
             filter: '', list01: 0, list02: 0, curPage: 1, update: 0,
@@ -34,20 +34,17 @@
                         : this.getProductsPerList(this.list1,this.list01,null,null)
                     : [];
             },
-            FilteredProductIDs(){ let filter = this.filter.toString().toLowerCase().trim(), IDs = this.ListProductIDs; return _.isEmpty(filter) ? IDs : getProductsFiltered(filter,IDs) },
+            FilteredProductIDs(){ let filter = this.filter.toString().toLowerCase().trim(), IDs = this.ListProductIDs; return _.isEmpty(filter) ? IDs : this.getProductsFiltered(filter,IDs) },
             PageVisibleProductIDs(){ let FilteredProductIDs = this.FilteredProductIDs; return FilteredProductIDs.slice(this.pageItemsStartIdx,this.pageItemsEndsIdx); },
-            items(){ let ItemIDs = this.PageVisibleProductIDs; return _.map(ItemIDs,(id) => products[id]) },
+            items(){ let ItemIDs = this.PageVisibleProductIDs; return _.map(ItemIDs,(id) => this.products[id]) },
             TotalPages(){ return Math.ceil(this.FilteredProductIDs.length/this.itemsPerPage) }
         },
         methods: {
             filterText(text){ this.curPage = 1; this.filter = text; },
+            getProductsFiltered(text,ids){ return _.filter(ids,id => { let narration = this.products[id] ? _.trim(this.products[id].narration).toLowerCase() : ''; return narration.includes(text) }) },
             listener0(data){ this.list01 = data; this.curPage = 1; },
             listener1(data){ this.list02 = data; this.curPage = 1; },
         },
-        created(){ products = CCache['products'].dataById(); }
-    }
-
-    function getProductsFiltered(text,ids){
-        return _.filter(ids,id => { let narration = products[id] ? _.trim(products[id].narration).toLowerCase() : ''; return narration.includes(text) });
+        created(){ this.CCacheDataPrepare({ table:'products',method:'dataById' }); }
     }
 </script>
