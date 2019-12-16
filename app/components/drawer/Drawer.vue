39d9e8f4ec59a3515c0812e28d2dbef66556aa6d
Firose <firumon@github.com>
2019-12-16 12:36:18 +0530

    Debug menu link removed

--- app/components/drawer/Drawer.vue
+++ app/components/drawer/Drawer.vue
@@ -1,13 +1,12 @@
 <!--suppress ALL -->
 <template>
     <RadSideDrawer ref="drawer" drawerLocation="Right" @drawerClosed="changeRadSideDrawerStatus(false)" :drawerContentSize="drawerContentSize" >
-        <GridLayout rows="auto,auto,auto,*,auto,auto" columns="*" ~drawerContent class="bcp">
+        <GridLayout rows="auto,auto,auto,*,auto" columns="*" ~drawerContent class="bcp">
             <DrawerTitleBar row="0" col="0"></DrawerTitleBar>
             <DrawerProfileBar row="1" col="0"></DrawerProfileBar>
             <DrawerHomeNavigation row="2"></DrawerHomeNavigation>
             <ScrollView row="3" col="0"><DrawerMenu></DrawerMenu></ScrollView>
             <TextBold row="4" col="0" @tap.native="userLogout" class="w-full text-center p-y-15 c-bg-white" verticalAlignment="center">LOGOUT</TextBold>
-            <TextRegular row="5" class="w-full text-center c-bg-grey" @tap.native="$navigateTo(require('./../Debug').default)"> DEBUG </TextRegular>
         </GridLayout>
         <StackLayout ~mainContent>
             <slot></slot>
