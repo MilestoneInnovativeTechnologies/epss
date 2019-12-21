86ed9ff17c4e9407397cefb9ca72bf81530a8a5c
Firose <firumon@github.com>
2019-12-21 17:30:40 +0530

    Key added to refresh once mounted

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -2,23 +2,24 @@
     <App :title="name">
         <UserStoresInfoWithIcon width="90%" :user="id"></UserStoresInfoWithIcon>
         <UserAreasInfoWithIcon width="90%" :user="id"></UserAreasInfoWithIcon>
-        <HomeWidgets width="90%"></HomeWidgets>
-        <HomeMenu class="m-t-20"></HomeMenu>
+        <HomeWidgets width="90%" :key="'hw-'+uKey"></HomeWidgets>
+        <HomeMenu class="m-t-20" :key="'hm-'+uKey"></HomeMenu>
         <AppButton @tap.native="userLogout" class="m-t-15 c-white">LOGOUT</AppButton>
     </App>
 </template>
 
 <script>
-    import {mapState, mapGetters, mapActions} from 'vuex'
+    import {mapState} from 'vuex'
     import {logoutMixin} from "../assets/scripts/mixins/logout";
     import {Login} from "../assets/scripts/navigations";
 
     export default {
         name: "Home",
         mixins: [logoutMixin],
+        data(){ return { uKey:0 } },
         computed: mapState('User', ['id','name']),
         mounted: function () {
-            this.$nextTick(() => this.id ? null : this.$navigateTo(Login,{ backstackVisible:false }))
+            this.$nextTick(() => this.id ? this.uKey++ : this.$navigateTo(Login,{ backstackVisible:false }))
         }
     }
 </script>
