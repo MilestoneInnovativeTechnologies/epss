fbfdcea2642f0095087dca5a7fd99df9a956e419
Firose <firumon@github.com>
2019-12-30 12:02:50 +0530

    Debugging modifications

--- app/assets/scripts/debugs.js
+++ app/assets/scripts/debugs.js
@@ -1,12 +1,12 @@
 import Vue from 'nativescript-vue';
 import VueDevtools from 'nativescript-vue-devtools'
 
-// global.FSM = require('tns-core-modules/file-system');
-// global.ImgSrc = require('tns-core-modules/image-source');
+global.FSM = require('tns-core-modules/file-system');
+global.ImgSrc = require('tns-core-modules/image-source');
 
 if(TNS_ENV !== 'production') {
   Vue.use(VueDevtools,{ host:'192.168.10.11' });
   Vue.config.silent = false;
 }
 
-// global.pt = new (require('./services/PrintTemplate').PrintTemplate)();
+global.pt = new (require('./services/PrintTemplate').PrintTemplate)();
