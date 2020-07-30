d9c21ecb75ff13ad0553526e778e6a910a88b7ea
Firose <firumon@github.com>
2020-07-30 13:03:05 +0530

    Align middle removed as to make it fix with small devices

--- app/components/misc/Setup.vue
+++ app/components/misc/Setup.vue
@@ -1,31 +1,29 @@
 <template>
     <App title="Setup Application" action="Setup Application" drawer="false" back="false" :actionProps="{ isEnabled:!busy }" @setup-application="doSetup">
-        <AlignMiddle>
-            <TextHeadingSub class="m-t-30" text="Device UUID" />
-            <TextField class="cp fsi" v-model="uuid" />
-            <TextRegular :textWrap="true">Provide this UUID to Milestone to get your device registered for using ePlus Smart Sale</TextRegular>
-            <TextRegular :textWrap="true" class="m-t-20">Once your device registered, please proceed to Setup Application</TextRegular>
-            <ActivityIndicator :busy="busy" class="m-t-20"></ActivityIndicator>
-            <StackLayout v-if="busy" class="m-t-20">
-                <TextHighlight class="m-t-20 w-full text-center" :text="'Downloading records. Completed: '+percentage+'%'" v-if="tasks['Init synchronizing app records']"></TextHighlight>
-                <TextHeadingSub>Tasks</TextHeadingSub>
-                <GridLayout coloums="*,auto" :rows="taskRows">
-                    <template v-for="(status,task,idx) in tasks">
-                        <FontIcon class="cp m-t-2" size="16" :row="idx" col="0" :name="status ? 'check_box' : 'check_box_outline_blank'"></FontIcon>
-                        <TextHighlight class="m-l-20" :row="idx" col="1">{{ task }}</TextHighlight>
-                    </template>
-                </GridLayout>
-            </StackLayout>
-            <StackLayout v-if="dwnTblsRows">
-                <TextHeadingSub>Tables</TextHeadingSub>
-                <WrapLayout>
-                    <StackLayout v-for="(status,table) in dwnTables" orientation="horizontal" :key="'dwn-table-'+table">
-                        <TextHighlight>{{ table | properCase }}</TextHighlight>
-                        <FontIcon class="cp m-t-2 m-r-20" size="16" :name="status ? 'check_box' : 'check_box_outline_blank'"></FontIcon>
-                    </StackLayout>
-                </WrapLayout>
-            </StackLayout>
-        </AlignMiddle>
+        <TextHeadingSub class="m-t-30" text="Device UUID" />
+        <TextField class="cp fsi" v-model="uuid" />
+        <TextRegular :textWrap="true" text="Provide this UUID to Milestone to get your device registered for using ePlus Smart Sale" />
+        <TextRegular :textWrap="true" class="m-t-20" text="Once your device registered, please proceed to Setup Application" />
+        <ActivityIndicator :busy="busy" class="m-t-20" />
+        <StackLayout v-if="busy" class="m-t-20">
+            <TextHighlight class="m-t-20 w-full text-center" :text="'Downloading records. Completed: '+percentage+'%'" v-if="tasks['Init synchronizing app records']"></TextHighlight>
+            <TextHeadingSub text="Tasks" />
+            <GridLayout coloums="*,auto" :rows="taskRows">
+                <template v-for="(status,task,idx) in tasks">
+                    <FontIcon class="cp m-t-2" size="16" :row="idx" col="0" :name="status ? 'check_box' : 'check_box_outline_blank'" />
+                    <TextHighlight class="m-l-20" :row="idx" col="1" :text="task" />
+                </template>
+            </GridLayout>
+        </StackLayout>
+        <StackLayout v-if="dwnTblsRows">
+            <TextHeadingSub text="Tables" />
+            <WrapLayout>
+                <StackLayout v-for="(status,table) in dwnTables" orientation="horizontal" :key="'dwn-table-'+table">
+                    <TextHighlight>{{ table | properCase }}</TextHighlight>
+                    <FontIcon class="cp m-t-2 m-r-20" size="16" :name="status ? 'check_box' : 'check_box_outline_blank'" />
+                </StackLayout>
+            </WrapLayout>
+        </StackLayout>
     </App>
 </template>
 
