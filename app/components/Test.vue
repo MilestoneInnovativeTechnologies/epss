48b81b65aa3203268df117d76bb9be371de08ded
Firose Hussain <github@firumon.com>
2020-09-28 12:14:44 +0530

    Adding un versioned files

---
+++ app/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+    <Page actionBarHidden="true" backgroundSpanUnderStatusBar="true">
+        <GridLayout rows="*" columns="*">
+            <label text="first" class="text-center bcp" verticalAlignment="center" />
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+    export default {
+        name: "Test"
+    }
+</script>
